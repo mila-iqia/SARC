74a681ab0c89aad44cea7b38158c0c4389714a46
Guillaume Alain <gyomalin@gmail.com>
2023-02-01 15:42:29 -0500

    adding files corresponding to secrets

---
+++ sarc/account_matching/make_matches.py
@@ -0,0 +1,217 @@
+"""
+This script will ingest three different sources of data and will produce
+a JSON file called "matches_done.json" that will contain a list of
+all the possible matches between Mila users and CC/DRAC accounts.
+
+There are a lot of irregularities in the worlds of accounts coming from Mila
+and from DRAC, so this script needs to have many exceptions written somewhere.
+Because those exceptions involve names of Mila members and their email addresses,
+we have to keep this private, despite how convenient it would be at times to
+have some statements like `if name == "johnappleseed@mila.quebec"`.
+
+These all need to come from a specific configuration file, and that file is include
+with the other secrets in the "secrets/account_matching" directory.
+"""
+
+
+
+import numpy as np
+import json
+import csv
+import os
+from collections import defaultdict
+from names_matching import find_exact_bag_of_words_matches
+
+# This config contains the following keys:
+#     'L_phantom_mila_emails_to_ignore'
+#     'D_override_matches_mila_to_cc_account_username'
+# which were handcrafted based on weird edge cases
+# that we got when we first wrote this script.
+config_path = "secrets/account_matching/make_matches_config.json"
+
+data_paths = {
+    "mila_ldap":  "secrets/account_matching/2022-11-26_mila_users.json",
+    "cc_members": "secrets/account_matching/members-rrg-bengioy-ad-2022-11-25.csv",
+    "cc_roles":   "secrets/account_matching/sponsored_roles_for_Yoshua Bengio (CCI_ jvb-000).csv",
+    }
+
+# Please be careful here because we currently don't have proper
+# source-version control for the secrets, and we don't want to
+# overwrite the tried-and-true "matches_done.json" file
+# when running this code for fun.
+output_file = "secrets/account_matching/matches_done.json"
+
+with open(config_path, "r") as f_in:
+    config = json.load(f_in)
+    S_phantom_mila_emails_to_ignore = set(config["L_phantom_mila_emails_to_ignore"])
+    D_override_matches_mila_to_cc_account_username = config["D_override_matches_mila_to_cc_account_username"]
+    del config
+
+# The cc_account_username in `D_override_matches_mila_to_cc_account_username`
+# refers to values found in the "cc_members" data source.
+
+
+def how_many_cc_accounts_with_mila_emails(data, key="cc_members"):
+    assert key in data
+    LD_members = [
+        D_member for D_member in data[key]
+        if D_member.get("email", "").endswith("@mila.quebec")]
+
+    print(f"We have {len(LD_members)} {key} accounts with @mila.quebec, out of {len(data['cc_members'])}.")
+    return LD_members
+
+
+
+
+def run():
+
+    def dict_to_lowercase(D):
+        return dict( (k.lower(), v) for (k, v) in D.items())
+
+    data = {}
+    for (k, v) in data_paths.items():
+        with open(v, "r") as f_in:
+            if v.endswith("csv"):
+                data[k] = [dict_to_lowercase(D) for D in csv.DictReader(f_in)]
+            elif v.endswith("json"):
+               data[k] = json.load(f_in)
+
+
+    # Filter out the "cc_members" whose "Activation_Status" is "older_deactivated" or "expired".
+    # These accounts might not have members present in the Mila LDAP.
+    # ex: francis.gregoire@mila.quebec is "older_deactivated"
+    #     luccionis@mila.quebec is "expired"
+    if "cc_members" in data:
+        data["cc_members"] = [D for D in data["cc_members"] if D["activation_status"] not in ["older_deactivated", "expired"]]
+        # because "Arian.Khorasani@mila.quebec" wrote their email with uppercases
+        for e in data["cc_members"]:
+            e["email"] = e["email"].lower()
+
+    if "cc_roles" in data:
+        data["cc_roles"] = [D for D in data["cc_roles"] if D["status"].lower() in ["activated"]]
+        # because "Arian.Khorasani@mila.quebec" wrote their email with uppercases
+        for e in data["cc_roles"]:
+            e["email"] = e["email"].lower()
+
+    # Dict indexed by @mila.quebec email addresses
+    # with 3 subdicts : "mila_ldap", "cc_roles", "cc_members"
+    # that contains all the information that we could match.
+    DD_persons = {}
+    for D in data["mila_ldap"]:
+        DD_persons[D["mila_email_username"]] = {}
+        DD_persons[D["mila_email_username"]]["mila_ldap"] = D
+        # filling those more for documentation purposes than anything
+        DD_persons[D["mila_email_username"]]["cc_roles"] = None
+        DD_persons[D["mila_email_username"]]["cc_members"] = None
+
+
+    ######################################
+    ## and now we start matching things ##
+    ######################################
+
+    for key in ["cc_members", "cc_roles"]:
+        if key not in data:
+            # we might not have all three source files
+            continue
+        LD_members = how_many_cc_accounts_with_mila_emails(data, key)
+        for D_member in LD_members:
+            assert D_member["email"].endswith("@mila.quebec")
+            if D_member["email"] in S_phantom_mila_emails_to_ignore:
+                print(f'Ignoring phantom {D_member["email"]}.')
+                continue
+            DD_persons[D_member["email"]][key] = D_member
+    # We have 206 cc_members accounts with @mila.quebec, out of 610.
+    # We have 42 cc_roles accounts with @mila.quebec, out of 610.
+
+
+    for (name_or_nom, key) in [("name", "cc_members"), ("nom", "cc_roles")]:
+        LP_name_matches = find_exact_bag_of_words_matches(
+            [e[name_or_nom] for e in data[key]],
+            [e["display_name"] for e in data["mila_ldap"]],
+        )
+        for (a, b, delta) in LP_name_matches:
+            if delta > 2:
+                # let's skip those
+                print(f"Skipped ({a}, {b}) because the delta is too large.")
+                continue
+            # Again with the O(N^2) matching.
+            for D_person in DD_persons.values():
+                if D_person["mila_ldap"]["display_name"] == b:
+                    D_person_found = D_person
+                    break
+            # We know for FACT that this person is in there,
+            # so when we `break`, then `D_person_found` is assigned.
+            # It becomes the insertion point.
+
+            # Again a strange construct that works because we know that
+            # there is a match in there with `e[name_or_nom] == a` because
+            # that's actually how we got it.
+            match = [e for e in data[key] if e[name_or_nom] == a][0]
+
+            # Matching names is less of a strong association than
+            # matching emails, so let's not mess things up by overwriting
+            # one by the other. It would still be interesting to report
+            # divergences here, where emails suggest a match that names don't.
+            if D_person_found.get(key, None) is not None:
+                # TODO : sanity check here if you want
+                # assert D_person_found[key] == match  # this?
+                continue
+            else:
+                D_person_found[key] = match
+
+
+    assert "cc_members" in data
+    matching = dict((e["username"], e) for e in data["cc_members"])
+    # The manual matching done previously by me.
+    for (mila_email_username, cc_account_username) in D_override_matches_mila_to_cc_account_username.items():
+        # If a key is missing here, it's because we messed up by writing
+        # by hand the values in `D_override_matches_mila_to_cc_account_username`.
+        DD_persons[mila_email_username]["cc_members"] = matching[cc_account_username]
+
+        
+
+
+    ###########################
+    ###### status report ######
+    ###########################
+
+    (good_count, bad_count, enabled_count, disabled_count) = (0, 0, 0, 0)
+    for D_person in DD_persons.values():
+        if D_person["mila_ldap"]["status"] == "enabled":
+            if D_person["cc_members"] is not None or D_person["cc_roles"] is not None:
+                good_count += 1
+            else:
+                bad_count += 1
+            enabled_count += 1
+        else:
+            disabled_count += 1
+
+    print(f"We have {enabled_count} enabled accounts and {disabled_count} disabled accounts.")
+    print(f"Out of those enabled accounts, there are {good_count} successful matches and {bad_count} failed matches.")
+
+
+
+    # TODO : report on how many of the CC entries couldn't be matches to mila LDAP
+
+    if "cc_members" in data:
+        count_cc_members_activated = len([D for D in data["cc_members"] if D["activation_status"] in ["activated"]])
+        print(f"We have {count_cc_members_activated} activated cc_members.")
+
+        # let's try to be more precise about things to find the missing accounts
+        set_A = set([D_member["email"] for D_member in data["cc_members"]
+                    if D_member["activation_status"] in ["activated"]])
+        set_B = set([D_person["cc_members"].get("email", None) for D_person in DD_persons.values() if D_person.get("cc_members", None) is not None])
+        print(f"We could not find matches in the Mila LDAP for the CC accounts associated with the following emails: {set_A.difference(set_B)}.")
+
+    # see "account_matching.md" for some explanations on the edge cases handled
+
+    if "cc_roles" in data:
+        count_cc_roles_activated = len([D for D in data["cc_roles"] if D["status"].lower() in ["activated"]])
+        print(f"We have {count_cc_roles_activated} activated cc_roles.")
+
+    with open(output_file, "w") as f_out:
+        json.dump(DD_persons, f_out, indent=2)
+        print(f"Wrote {output_file}.")
+
+if __name__ == "__main__":
+    run()
---
+++ sarc/ldap/read_mila_ldap.py
@@ -0,0 +1,421 @@
+"""
+What this script does
+=====================
+
+This script runs locally every day on a machine at Mila.
+It queries our LDAP service for all users and it updates
+the MongoDB instance for Clockwork so that the "users"
+collection reflects those accounts.
+
+It's unclear whether we should automatically disable
+the existing accounts in CW for members that are not being
+mentioned in the LDAP results. We probably want to do
+such a thing periodically, with special care instead of
+doing it automatically.
+
+Two mutually-exclusive ways to input the data, by priority:
+   1) use the --input_json_file argument
+   2) use the LDAP
+
+Two ways to output the data, that can be used together:
+   1) to the --output_json_file
+   2) to a MongoDB instance
+
+
+Useful note for later about "clockwork_api_key"
+===============================================
+
+The two extra fields "clockwork_api_key" and "cc_account_username"
+are added when the entries are committed to the database,
+but not when they are read or written to a json file.
+
+
+Sample uses
+===========
+
+Two ways this can be used:
+
+::
+
+    python3 read_mila_ldap.py \\
+        --local_private_key_file mila_ldap_readonly_credentials/Google_2025_01_26_68154.key \\
+        --local_certificate_file mila_ldap_readonly_credentials/Google_2025_01_26_68154.crt \\
+        --ldap_service_uri ldaps://ldap.google.com \\
+        --mongodb_connection_string ${MONGODB_CONNECTION_STRING} \\
+        --output_json_file mila_users.json
+
+    python3 read_mila_ldap.py \\
+        --mongodb_connection_string ${MONGODB_CONNECTION_STRING} \\
+        --input_json_file mila_users.json
+
+
+LDAP data structure
+===================
+
+When it comes to the LDAP entries that we get (and need to process),
+they are structured as follows:
+
+::
+
+    {
+        "attributes": {
+            "apple-generateduid": [
+                "AF54098F-29AE-990A-B1AC-F63F5A89B89"
+            ],
+            "cn": [
+                "john.smith",
+                "John Smith"
+            ],
+            "departmentNumber": [],
+            "displayName": [
+                "John Smith"
+            ],
+            "employeeNumber": [],
+            "employeeType": [],
+            "gecos": [
+                ""
+            ],
+            "gidNumber": [
+                "1500000001"
+            ],
+            "givenName": [
+                "John"
+            ],
+            "googleUid": [
+                "john.smith"
+            ],
+            "homeDirectory": [
+                "/home/john.smith"
+            ],
+            "loginShell": [
+                "/bin/bash"
+            ],
+            "mail": [
+                "john.smith@mila.quebec"
+            ],
+            "memberOf": [],
+            "objectClass": [
+                "top",
+                "person",
+                "organizationalPerson",
+                "inetOrgPerson",
+                "posixAccount"
+            ],
+            "physicalDeliveryOfficeName": [],
+            "posixUid": [
+                "smithj"
+            ],
+            "sn": [
+                "Smith"
+            ],
+            "suspended": [
+                "false"
+            ],
+            "telephoneNumber": [],
+            "title": [],
+            "uid": [
+                "john.smith"
+            ],
+            "uidNumber": [
+                "1500000001"
+            ]
+        },
+        "dn": "uid=john.smith,ou=IDT,ou=STAFF,ou=Users,dc=mila,dc=quebec"
+    }
+
+"""
+
+import os
+import argparse
+import json
+
+# Requirements
+# - pip install ldap3
+from ldap3 import Server, Connection, Tls, ALL_ATTRIBUTES, SUBTREE
+import ssl
+
+from pymongo import MongoClient, UpdateOne
+
+
+parser = argparse.ArgumentParser(
+    description="Query LDAP and update the MongoDB database users based on values returned."
+)
+parser.add_argument(
+    "--local_private_key_file",
+    type=str,
+    help="local_private_key_file for LDAP connection",
+)
+parser.add_argument(
+    "--local_certificate_file",
+    type=str,
+    help="local_certificate_file for LDAP connection",
+)
+parser.add_argument(
+    "--ldap_service_uri",
+    type=str,
+    default="ldaps://ldap.google.com",
+    help="ldap service uri",
+)
+# We have two possible things that we can do with the data fetched.
+# Dumping to a json file is possible.
+parser.add_argument(
+    "--mongodb_connection_string",
+    default=None,
+    type=str,
+    help="(optional) MongoDB connection string. Contains username and password.",
+)
+parser.add_argument(
+    "--mongodb_database",
+    default="clockwork",
+    type=str,
+    help="(optional) MongoDB database to modify. Better left at default.",
+)
+parser.add_argument(
+    "--mongodb_collection",
+    default="users",
+    type=str,
+    help="(optional) MongoDB collection to modify. Better left at default.",
+)
+parser.add_argument(
+    "--input_json_file",
+    default=None,
+    type=str,
+    help="(optional) Ignore the LDAP and load from this json file instead.",
+)
+parser.add_argument(
+    "--output_json_file",
+    default=None,
+    type=str,
+    help="(optional) Write results to json file.",
+)
+parser.add_argument(
+    "--output_raw_LDAP_json_file",
+    default=None,
+    type=str,
+    help="(optional) Write results of the raw LDAP query to json file.",
+)
+
+
+def query_ldap(local_private_key_file, local_certificate_file, ldap_service_uri):
+    """
+    Since we don't always query the LDAP (i.e. omitted when --input_json_file is given),
+    we'll make this a separate function.
+    """
+
+    assert os.path.exists(
+        local_private_key_file
+    ), f"Missing local_private_key_file {local_private_key_file}."
+    assert os.path.exists(
+        local_certificate_file
+    ), f"Missing local_certificate_file {local_certificate_file}."
+
+    # Prepare TLS Settings
+    tls_conf = Tls(
+        local_private_key_file=local_private_key_file,
+        local_certificate_file=local_certificate_file,
+        validate=ssl.CERT_REQUIRED,
+        version=ssl.PROTOCOL_TLSv1_2,
+    )
+    # Connect to LDAP
+    server = Server(ldap_service_uri, use_ssl=True, tls=tls_conf)
+    conn = Connection(server)
+    conn.open()
+    # Extract all the data
+    conn.search(
+        "dc=mila,dc=quebec",
+        "(objectClass=inetOrgPerson)",
+        search_scope=SUBTREE,
+        attributes=ALL_ATTRIBUTES,
+    )
+    # We make the decision here to return only the "attributes"
+    # and leave out the "dn" field.
+    return [json.loads(entry.entry_to_json())["attributes"] for entry in conn.entries]
+
+
+def process_user(user_raw: dict) -> dict:
+    """
+    This takes a dict with a LOT of fields, as described by GEN-1744,
+    and it uses only the following ones, which are renamed.
+    Note that all entries from `user_raw` are lists, and we expect
+    them to contain only one element at [0].
+
+    mail[0]        -> mila_email_username  (includes the "@mila.quebec")
+    posixUid[0]    -> mila_cluster_username
+    uidNumber[0]   -> mila_cluster_uid
+    gidNumber[0]   -> mila_cluster_gid
+    displayName[0] -> display_name
+    suspended[0]   -> status  (as string "enabled" or "disabled")
+
+    It also asserts, as sanity check, that the entries for
+    "googleUid" and "uid" match that of "mail" (except for
+    the "@mila.quebec" suffix).
+    """
+    user = {
+        # include the suffix "@mila.quebec"
+        "mila_email_username": user_raw["mail"][0],
+        "mila_cluster_username": user_raw["posixUid"][0],
+        "mila_cluster_uid": user_raw["uidNumber"][0],
+        "mila_cluster_gid": user_raw["gidNumber"][0],
+        "display_name": user_raw["displayName"][0],
+        "status": "disabled"
+        if (user_raw["suspended"][0] in ["True", "true", True])
+        else "enabled",
+    }
+    assert user_raw["mail"][0].startswith(user_raw["googleUid"][0])
+    assert user_raw["mail"][0].startswith(user_raw["uid"][0])
+    return user
+
+
+def client_side_user_updates(LD_users_DB, LD_users_LDAP):
+    """
+    Instead of having complicated updates that depend on multiple MongoDB
+    updates to cover all cases involving the "status" field, we'll do all
+    that logic locally in this function.
+
+    We have `LD_users_DB` from our database, we have `LD_users_LDAP` from
+    our LDAP server, and then we return list of updates to be commited.
+
+    Note that both `LD_users_DB` and `LD_users_LDAP` use the same fields.
+    """
+    # The first step is to index everything by unique id, which is
+    # the "mila_email_username". This is because we'll be matching
+    # entries from both lists and we want to avoid N^2 performance.
+
+    DD_users_DB = dict((e["mila_email_username"], e) for e in LD_users_DB)
+    DD_users_LDAP = dict((e["mila_email_username"], e) for e in LD_users_LDAP)
+
+    LD_users_to_update_or_insert = []
+    for meu in set(list(DD_users_DB.keys()) + list(DD_users_LDAP.keys())):
+        # `meu` is short for the mila_email_username value
+
+        if meu in DD_users_DB and not meu in DD_users_LDAP:
+            # User is in DB but not in the LDAP. Don't change it in DB.
+            # Don't even update it. This situation is an exception
+            # what might happen for special admin accounts or something,
+            # but never with regular student accounts.
+            continue
+        elif meu not in DD_users_DB and meu in DD_users_LDAP:
+            # User is not in DB but is in the LDAP. That's a new entry!
+            # We want to dress it up and commit it to the DB.
+            entry = DD_users_LDAP[meu]
+            entry["cc_account_username"] = None
+            entry["clockwork_api_key"] = None
+            entry["cc_account_update_key"] = None
+            # Any web_settings not present will pull values
+            # from `get_default_web_settings_values()` later down
+            # the road. It would actually be better to leave them out
+            # at this point and not fill them with anything hardcoded.
+            # Just make sure you make this a dict, though.
+            entry["web_settings"] = {}
+            assert "status" in entry  # sanity check
+        else:
+            # User is in DB and in LDAP. Update it carefully and don't
+            # disturb the fields that shouldn't be touched.
+            entry = DD_users_DB[meu]
+            if DD_users_LDAP[meu]["status"] == "disabled":
+                # No matter what we said in the database, if the LDAP
+                # says that it's disabled, then we propagate that change.
+                # We wouldn't do the same thing with "enabled" in the LDAP, though.
+                entry["status"] = "disabled"
+            assert "cc_account_username" in entry  # sanity check
+            assert "clockwork_api_key" in entry  # sanity check
+
+        LD_users_to_update_or_insert.append(entry)
+    return LD_users_to_update_or_insert
+
+
+def run(
+    local_private_key_file=None,
+    local_certificate_file=None,
+    ldap_service_uri=None,
+    mongodb_connection_string=None,
+    mongodb_database=None,
+    mongodb_collection=None,
+    input_json_file=None,
+    output_json_file=None,
+    output_raw_LDAP_json_file=None,
+    LD_users=None,  # for external testing purposes
+):
+
+    if LD_users is not None:
+        # Used mostly for testing purposes.
+        # Overrides the "input_json_file" argument.
+        # Just make sure it's a list of dict, at least.
+        assert isinstance(LD_users, list)
+        if LD_users:
+            assert isinstance(LD_users[0], dict)
+    elif input_json_file:
+        with open(input_json_file, "r") as f_in:
+            LD_users = json.load(f_in)
+    else:
+        # this is the usual branch taken in practice
+        LD_users_raw = query_ldap(
+            local_private_key_file, local_certificate_file, ldap_service_uri
+        )
+        if output_raw_LDAP_json_file:
+            with open(output_raw_LDAP_json_file, "w") as f_out:
+                json.dump(LD_users_raw, f_out, indent=4)
+                print(f"Wrote {output_raw_LDAP_json_file}.")
+
+        LD_users = [process_user(D_user_raw) for D_user_raw in LD_users_raw]
+
+    if mongodb_connection_string and mongodb_database and mongodb_collection:
+
+        users_collection = MongoClient(mongodb_connection_string)[mongodb_database][
+            mongodb_collection
+        ]
+
+        # The "enabled" component has to be dealt with differently.
+        #
+        # For a user that already exists in our database,
+        #   - if the LDAP says "disabled", then we propagate that to our database;
+        #   - if the LDAP says "enabled", then we ignore it.
+        # For a user that is not in our database,
+        #   - we go with whatever the LDAP says.
+
+        LD_users_DB = list(users_collection.find())
+
+        L_updated_users = client_side_user_updates(
+            LD_users_DB=LD_users_DB, LD_users_LDAP=LD_users
+        )
+
+        L_updates_to_do = [
+            UpdateOne(
+                {"mila_email_username": updated_user["mila_email_username"]},
+                {
+                    # We set all the fields corresponding to the fields from `updated_user`,
+                    # so that's a convenient way to do it. Note that this does not affect
+                    # the fields in the database that are already present for that user.
+                    "$set": updated_user,
+                },
+                upsert=True,
+            )
+            for updated_user in L_updated_users
+        ]
+
+        if L_updates_to_do:
+            result = users_collection.bulk_write(
+                L_updates_to_do
+            )  #  <- the actual commit
+            # print(result.bulk_api_result)
+
+    if output_json_file:
+        with open(output_json_file, "w") as f_out:
+            json.dump(LD_users, f_out, indent=4)
+            print(f"Wrote {output_json_file}.")
+
+
+if __name__ == "__main__":
+
+    args = parser.parse_args()
+    run(
+        local_private_key_file=args.local_private_key_file,
+        local_certificate_file=args.local_certificate_file,
+        ldap_service_uri=args.ldap_service_uri,
+        mongodb_connection_string=args.mongodb_connection_string,
+        mongodb_database=args.mongodb_database,
+        mongodb_collection=args.mongodb_collection,
+        input_json_file=args.input_json_file,
+        output_json_file=args.output_json_file,
+        output_raw_LDAP_json_file=args.output_raw_LDAP_json_file,
+    )
