19ebe1fbc7053731f6f0ac3d27c628173d310cbf
Olivier Breuleux <breuleux@gmail.com>
2023-02-10 14:45:56 -0500

    Move sacct-acquire script and allow writing multiple days

---
+++ sarc/jobs/acquire.py
@@ -0,0 +1,19 @@
+import sys
+from datetime import datetime
+
+from sarc.config import config
+from sarc.jobs.db import sacct_mongodb_import
+
+
+def main():
+    cluster_name, *date_strings = sys.argv[1:]
+    cfg = config()
+    mc = cfg.clusters[cluster_name]
+    for date_string in date_strings:
+        date = datetime.strptime(date_string, "%Y-%m-%d")
+        print(f"Acquire data for date: {date}")
+        sacct_mongodb_import(mc, date)
+
+
+if __name__ == "__main__":
+    main()
