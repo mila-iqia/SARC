197bf980bac5d7fb8ea9f882dc8a06f4af6ec551
Bruno Carrez <bruno.carrez@mila.quebec>
2023-03-01 10:47:17 -0500

    rm quick and dirty test/helloworld code from repo

--- examples/disk_usage.py
+++ examples/disk_usage.py
@@ -1,14 +1,33 @@
 from sarc.config import config
-from sarc.inode_storage_scanner.get_diskusage import *
+from sarc.storage.drac import *
 
 # Get disk usage of DRAC clusters
 
-# drac_clusters=["narval", "beluga", "cedar", "graham"]
-drac_clusters=["narval"]
+drac_clusters=["narval", "beluga" , "cedar", "graham"]
+# drac_clusters=["narval"]
+
+drac_reports={}
 
 for cluster_name in drac_clusters:
-    cluster = config().clusters[cluster_name]
-    print (f"fetching data from cluster {cluster.host} ...")
-    report = fetch_diskusage_report(cluster)
-    print (report[0:34])
+    print(f"Parsing {cluster_name} report...")
+
+    # cluster = config().clusters[cluster_name]
+    # print (f"fetching data from cluster {cluster.host} ...")
+    # report = fetch_diskusage_report(cluster)
+
+    f=open(f"report_{cluster_name}.txt","r")
+    report = f.readlines()
+    f.close()
+
+    # f=open(f"report_{cluster_name}.txt","w")
+    # for line in report:
+    #     f.write(f"{line}\n")
+    # f.close()
+
+    header,body = parse_diskusage_report(report)
+    # print(header)
+    for k in body:
+        print (k)
+        print (body[k])
+    # print (body)
 
