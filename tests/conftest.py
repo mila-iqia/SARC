87bd2144c07971843e827b7311413da404971a81
Arnaud Bergeron <abergeron@gmail.com>
2023-03-01 16:09:41 -0500

    test fixtures to help test functions that will perform ssh

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,9 @@ from pathlib import Path
 
 import pytest
 
-from sarc.config import parse_config, using_config
+pytest_plugins = "fabric.testing.fixtures"
+
+from sarc.config import ClusterConfig, Config, MongoConfig, parse_config, using_config
 
 
 @pytest.fixture(scope="session")
@@ -21,3 +23,35 @@ def use_standard_config(standard_config_object):  # pylint: disable=redefined-ou
 def tzlocal_is_mtl(monkeypatch):
     monkeypatch.setattr("sarc.config.TZLOCAL", zoneinfo.ZoneInfo("America/Montreal"))
     monkeypatch.setattr("sarc.jobs.job.TZLOCAL", zoneinfo.ZoneInfo("America/Montreal"))
+
+
+@pytest.fixture
+def test_config(
+    tmp_path,
+    request,
+):
+    vals = getattr(request, "param", dict())
+
+    mongo_url = vals.get("mongo_url", "localhost:27017")
+    mongo_database = vals.get("mongo_database", "sarc-test")
+    cluster_name = vals.get("cluster_name", "test")
+    cluster_host = vals.get("cluster_host", "test")
+    cluster_timezone = vals.get("cluster_timezone", "America/Montreal")
+    cluster_sacct_bin = vals.get("cluster_sacct_bin", "sacct")
+    cluster_accounts = vals.get("cluster_accounts", None)
+
+    mongo_conf = MongoConfig(url=mongo_url, database=mongo_database)
+    cluster_conf = ClusterConfig(
+        host=cluster_host,
+        timezone=cluster_timezone,
+        sacct_bin=cluster_sacct_bin,
+        accounts=cluster_accounts,
+    )
+    conf = Config(
+        mongo=mongo_conf,
+        sshconfig=None,
+        cache=tmp_path,
+        clusters={cluster_name: cluster_conf},
+    )
+    with using_config(conf):
+        yield conf
