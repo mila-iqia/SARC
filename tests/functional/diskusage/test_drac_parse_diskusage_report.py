0124b94318f2f5a47601dcc5e03a33d97ed1372a
Bruno Carrez <bruno.carrez@mila.quebec>
2023-03-03 14:14:37 -0500

    update tests to use file_regression

--- tests/functional/diskusage/test_drac_parse_diskusage_report.py
+++ tests/functional/diskusage/test_drac_parse_diskusage_report.py
@@ -6,60 +6,24 @@ from sarc.storage.drac import _parse_header_summary as parse_header_summary
 
 
 # cedar, beluga and graham style
-def test_header_00():
+def test_header_00(file_regression):
     f = open(Path(__file__).parent / "drac_reports/report_gerudo.txt", "r")
     report = f.readlines()
     f.close()
-    L_results = parse_header_summary(report)
-    L_results_expected = [
-        {"group": "def-bengioy", "space": "971G/1000G", "nbr_files": "791k/1005k"},
-        {"group": "rpp-bengioy", "space": "31T/2048k", "nbr_files": "3626k/1025"},
-    ]
-
-    assert len(L_results) == len(L_results_expected)
-
-    for a, b in zip(L_results, L_results_expected):
-        assert a == b
+    file_regression.check('\n'.join(map(str,parse_header_summary(report))))
 
 
 # narval style
-def test_header_01():
+def test_header_01(file_regression):
     f = open(Path(__file__).parent / "drac_reports/report_hyrule.txt", "r")
     report = f.readlines()
     f.close()
-    L_results = parse_header_summary(report)
-    L_results_expected = [
-        {"group": "rrg-bengioy-ad", "space": "39T/75T", "nbr_files": "1316k/5000k"},
-        {"group": "def-bengioy", "space": "956G/1000G", "nbr_files": "226k/500k"},
-    ]
-
-    assert len(L_results) == len(L_results_expected)
-
-    for a, b in zip(L_results, L_results_expected):
-        assert a == b
+    file_regression.check('\n'.join(map(str,parse_header_summary(report))))
 
 
-def test_parse_body():
+
+def test_parse_body(file_regression):
     f = open(Path(__file__).parent / "drac_reports/report_gerudo.txt", "r")
     report = f.readlines()
     f.close()
-
-    DLD_results = parse_body(report)
-    assert set(list(DLD_results.keys())) == set(["def-bengioy", "rpp-bengioy"])
-
-    assert DLD_results["def-bengioy"] == [
-        {"username": "revali", "nbr_files": 2, "size": "0.00 GiB"},
-        {"username": "urbosa", "nbr_files": 2, "size": "0.00 GiB"},
-        {"username": "daruk", "nbr_files": 50, "size": "13.49 GiB"},
-        {"username": "mipha", "nbr_files": 2, "size": "0.00 GiB"},
-        {"username": "Total", "nbr_files": 696928, "size": "877.51 GiB"},
-    ]
-
-    assert DLD_results["rpp-bengioy"] == [
-        {"username": "riju", "nbr_files": 47085, "size": "4.20 GiB"},
-        {"username": "grosaillieh", "nbr_files": 2, "size": "0.00 GiB"},
-        {"username": "bourgette", "nbr_files": 2, "size": "0.00 GiB"},
-        {"username": "kohga", "nbr_files": 2, "size": "0.00 GiB"},
-        {"username": "rhoam", "nbr_files": 2, "size": "0.00 GiB"},
-        {"username": "Total", "nbr_files": 3626455, "size": "30009.08 GiB"},
-    ]
+    file_regression.check(str(parse_body(report)))
