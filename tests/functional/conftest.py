84598eaf2f5405809b5d47ccb47f998acd8fe7d0
Bruno Carrez <bruno.carrez@mila.quebec>
2023-03-06 12:17:47 -0500

    tests fonction get_diskusage

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -276,6 +276,42 @@ base_job = {
 }
 
 
+def create_diskusages():
+    diskusages = []
+    for cluster_name in ["botw", "totk"]:
+        for timestamp in [
+            datetime(2023, 2, 14, 0, 0, 0, tzinfo=UTC),
+            datetime(2021, 12, 1, 0, 0, 0, tzinfo=UTC),
+        ]:
+            diskusages.append(
+                {
+                    "cluster_name": cluster_name,
+                    "timestamp": timestamp,
+                    "groups": [
+                        {
+                            "group_name": "gerudo",
+                            "users": [
+                                {"user": "urbosa", "nbr_files": 2, "size": 0},
+                                {"user": "riju", "nbr_files": 50, "size": 14484777205},
+                                {"user": "mipha", "nbr_files": 2, "size": 0},
+                            ],
+                        },
+                        {
+                            "group_name": "piaf",
+                            "users": [
+                                {
+                                    "user": "revali",
+                                    "nbr_files": 47085,
+                                    "size": 4509715660,
+                                },
+                            ],
+                        },
+                    ],
+                }
+            )
+    return diskusages
+
+
 class JobFactory:
     def __init__(
         self, first_submit_time: None | datetime = None, first_job_id: int = 1
@@ -393,6 +429,7 @@ def custom_db_config(cfg, db_name):
 def fill_db(db):
     db.allocations.insert_many(create_allocations())
     db.jobs.insert_many(create_jobs())
+    db.diskusage.insert_many(create_diskusages())
 
 
 def create_db_configuration_fixture(db_name, empty=False, scope="function"):
