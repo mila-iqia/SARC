58b8a69a978616e341eba687d53ebeaacf5ab229
Bruno Carrez <bruno.carrez@mila.quebec>
2023-03-06 14:28:42 -0500

    cli storages (acquire from file)

--- sarc/cli/acquire/storages.py
+++ sarc/cli/acquire/storages.py
@@ -1,12 +1,16 @@
 from dataclasses import dataclass
+from pathlib import Path
 
 from simple_parsing import field
 
 from sarc.config import config
+from sarc.storage.diskusage import get_diskusage_collection
+from sarc.storage.drac import convert_parsed_report_to_diskusage, parse_diskusage_report
 
 
 @dataclass
 class AcquireStorages:
+    file: Path
     cluster_names: list[str] = field(
         alias=["-c"], default_factory=list, choices=list(config().clusters.keys())
     )
@@ -16,8 +20,21 @@ class AcquireStorages:
         clusters_configs = cfg.clusters  # pylint: disable=unused-variable
 
         for cluster_name in self.cluster_names:
-            print(f"Acquire data on {cluster_name}.")
-            # storage_mongodb_import(clusters_configs[cluster_name])
-            raise NotImplementedError()
+
+            print(f"Acquiring {cluster_name} storages report...")
+
+            f = open(self.file, "r")
+            report = f.readlines()
+            f.close()
+            header, body = parse_diskusage_report(report)
+
+            print(body)
+
+            du = convert_parsed_report_to_diskusage(cluster_name, body)
+            
+            collection = get_diskusage_collection()
+            collection.add(du)
+
+            # raise NotImplementedError() 
 
         return 0
