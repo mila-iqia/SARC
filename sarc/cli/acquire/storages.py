0abc14aa55e90be2eb43a163cac257b3dd307e61
Bruno Carrez <bruno.carrez@mila.quebec>
2023-03-06 20:22:17 -0500

    without the '--file' parameter, cli storages acquire command fetches data from the cluster via ssh

--- sarc/cli/acquire/storages.py
+++ sarc/cli/acquire/storages.py
@@ -5,12 +5,12 @@ from simple_parsing import field
 
 from sarc.config import config
 from sarc.storage.diskusage import get_diskusage_collection
-from sarc.storage.drac import convert_parsed_report_to_diskusage, parse_diskusage_report
+from sarc.storage.drac import convert_parsed_report_to_diskusage, fetch_diskusage_report, parse_diskusage_report
 
 
 @dataclass
 class AcquireStorages:
-    file: Path
+    file: Path =field(default=None)
     cluster_names: list[str] = field(
         alias=["-c"], default_factory=list, choices=list(config().clusters.keys())
     )
@@ -22,9 +22,16 @@ class AcquireStorages:
         for cluster_name in self.cluster_names:
             print(f"Acquiring {cluster_name} storages report...")
 
-            f = open(self.file, "r")
-            report = f.readlines()
-            f.close()
+            report=None
+
+            if self.file:
+                f = open(self.file, "r")
+                report = f.readlines()
+                f.close()
+            else:
+                cluster = config().clusters[cluster_name]
+                report = fetch_diskusage_report(cluster)
+
             header, body = parse_diskusage_report(report)
 
             du = convert_parsed_report_to_diskusage(cluster_name, body)
