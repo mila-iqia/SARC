e352e1036b55a19fbf368b5b9d3d04c60502b6a0
Arnaud Bergeron <abergeron@gmail.com>
2023-03-07 15:35:38 -0500

    Add db init command to sarc

---
+++ sarc/cli/db/init.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from sarc.config import config
+
+
+@dataclass
+class DbInit:
+    url: Optional[str]
+    database: Optional[str]
+
+    def execute(self) -> int:
+        from pymongo import MongoClient
+
+        cfg = config()
+        url = cfg.mongo.url if self.url is None else self.url
+        database = cfg.mongo.database if self.database is None else self.database
+
+        client = MongoClient(url)
+        db = client.get_database(database)
+
+        # There is no need to create the collections first, they will be
+        # created when the index is created.
+        db.jobs.create_index(
+            [
+                ("cluster_name", pymongo.ASCENDING),
+                ("submit_time", pymongo.ASCENDING),
+                ("end_time", pymongo.ASCENDING),
+            ],
+        )
+
+        return 0
