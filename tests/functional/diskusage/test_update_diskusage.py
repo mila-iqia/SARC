40151073e1fb14ece6963a04d7cbdb78012e70c7
Bruno Carrez <bruno.carrez@mila.quebec>
2023-03-09 16:29:42 -0500

    CLI storages (#28)
    * cli storages (acquire from file)
    
    * update diskusage tests to use cli to acquire test data into db
    
    * removed unwanted print
    
    * without the '--file' parameter, cli storages acquire command fetches data from the cluster via ssh
    
    * test_drac_fetch_diskusage_report (use of ssh mock)
    
    * drac.fetch_diskusage_report: use cluster.diskusage_report_command instead of hard-coded text
    
    * update tests to use test cluster definitions in sarc-test.json !
    
    * removed dead commented code
    
    * add test test_drac_acquire_storages
    
    * removed debug prints
    
    * update CI policy
    
    * removed sshconfig from sarc-test.json
    
    * Update sarc/cli/acquire/storages.py
    
    Co-authored-by: Xavier Bouthillier <xavier.bouthillier@gmail.com>
    
    * woopsie
    
    ---------
    
    Co-authored-by: Xavier Bouthillier <xavier.bouthillier@gmail.com>

--- tests/functional/diskusage/test_update_diskusage.py
+++ tests/functional/diskusage/test_update_diskusage.py
@@ -2,30 +2,28 @@ import os
 
 import pytest
 
+from sarc.config import config
 from sarc.storage.diskusage import get_diskusage_collection, get_diskusages
 from sarc.storage.drac import convert_parsed_report_to_diskusage, parse_diskusage_report
 
 FOLDER = os.path.dirname(os.path.abspath(__file__))
 
 
-def import_file_to_db(clustername, filename):
-    """
-    utility function that inserts a diskusage text output of DRAC into mongo
-    """
-    f = open(os.path.join(FOLDER, filename), "r")
-    report = f.readlines()
-    f.close()
-    header, body = parse_diskusage_report(report)
-    collection = get_diskusage_collection()
-    collection.add(convert_parsed_report_to_diskusage(clustername, body))
-
-
 @pytest.mark.usefixtures("empty_read_write_db")
 @pytest.mark.freeze_time("2023-05-12")
-def test_update_drac_diskusage_one(file_regression):
+def test_update_drac_diskusage_one(file_regression, cli_main):
     assert get_diskusages(cluster_name=["gerudo", "hyrule"]) == []
 
-    import_file_to_db("gerudo", "drac_reports/report_gerudo.txt")
+    cli_main(
+        [
+            "acquire",
+            "storages",
+            "--file",
+            os.path.join(FOLDER, "drac_reports/report_gerudo.txt"),
+            "-c",
+            "gerudo",
+        ]
+    )
 
     data = get_diskusages(cluster_name=["gerudo", "hyrule"])
     assert len(data) == 1
@@ -34,10 +32,28 @@ def test_update_drac_diskusage_one(file_regression):
 
 @pytest.mark.usefixtures("empty_read_write_db")
 @pytest.mark.freeze_time("2023-05-12")
-def test_update_drac_diskusage_two(file_regression):
+def test_update_drac_diskusage_two(file_regression, cli_main):
     assert get_diskusages(cluster_name=["gerudo", "hyrule"]) == []
-    import_file_to_db("gerudo", "drac_reports/report_gerudo.txt")
-    import_file_to_db("hyrule", "drac_reports/report_hyrule.txt")
+    cli_main(
+        [
+            "acquire",
+            "storages",
+            "--file",
+            os.path.join(FOLDER, "drac_reports/report_gerudo.txt"),
+            "-c",
+            "gerudo",
+        ]
+    )
+    cli_main(
+        [
+            "acquire",
+            "storages",
+            "--file",
+            os.path.join(FOLDER, "drac_reports/report_hyrule.txt"),
+            "-c",
+            "hyrule",
+        ]
+    )
     data = get_diskusages(cluster_name=["gerudo", "hyrule"])
     assert len(data) == 2
     data_json = "\n".join(
@@ -51,11 +67,29 @@ def test_update_drac_diskusage_two(file_regression):
 
 @pytest.mark.usefixtures("empty_read_write_db")
 @pytest.mark.freeze_time("2023-05-12")
-def test_update_drac_diskusage_no_duplicate(file_regression):
+def test_update_drac_diskusage_no_duplicate(file_regression, cli_main):
     assert get_diskusages(cluster_name=["gerudo", "hyrule"]) == []
 
-    import_file_to_db("gerudo", "drac_reports/report_gerudo.txt")
-    import_file_to_db("gerudo", "drac_reports/report_gerudo.txt")
+    cli_main(
+        [
+            "acquire",
+            "storages",
+            "--file",
+            os.path.join(FOLDER, "drac_reports/report_gerudo.txt"),
+            "-c",
+            "gerudo",
+        ]
+    )
+    cli_main(
+        [
+            "acquire",
+            "storages",
+            "--file",
+            os.path.join(FOLDER, "drac_reports/report_gerudo.txt"),
+            "-c",
+            "gerudo",
+        ]
+    )
 
     data = get_diskusages(cluster_name=["gerudo", "hyrule"])
     assert len(data) == 1
