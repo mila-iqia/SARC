444867d6075c0051b0540cf119329bf98916a2b1
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2023-03-10 15:51:13 -0500

    Add db inits

--- sarc/cli/db/init.py
+++ sarc/cli/db/init.py
@@ -1,7 +1,12 @@
 from dataclasses import dataclass
 from typing import Optional
 
+import pymongo
+
+from sarc.allocations.allocations import AllocationsRepository
 from sarc.config import config
+from sarc.jobs.job import SlurmJobRepository
+from sarc.storage.diskusage import ClusterDiskUsageRepository
 
 
 @dataclass
@@ -10,7 +15,6 @@ class DbInit:
     database: Optional[str]
 
     def execute(self) -> int:
-        import pymongo
 
         cfg = config()
         url = cfg.mongo.url if self.url is None else self.url
@@ -21,12 +25,135 @@ class DbInit:
 
         # There is no need to create the collections first, they will be
         # created when the index is created.
-        db.jobs.create_index(
-            [
-                ("cluster_name", pymongo.ASCENDING),
-                ("submit_time", pymongo.ASCENDING),
-                ("end_time", pymongo.ASCENDING),
-            ],
-        )
+
+        # NOTE: Compound indices with mongodb provide subsets of compound index using prefix.
+        #       Ex: For the compound index (a, b, c), we also get compound indices (a, b) and (a)
+        #       But we don't get (b, c) or (c), which we must create explicitly if needed.
+
+        create_jobs_indices(db)
+
+        create_storages_indices(db)
+
+        create_allocations_indices(db)
+
+        create_users_indices(db)
 
         return 0
+
+
+def create_users_indices(db):
+
+    # db_collection = UserRepository(database=db).get_collection()
+    db_collection = db.users
+
+    db_collection.create_index([("mila_ldap.mila_email_username", pymongo.ASCENDING)])
+    db_collection.create_index([("mila_ldap.mila_cluster_username", pymongo.ASCENDING)])
+    db_collection.create_index(
+        [
+            ("cc_roles.username", pymongo.ASCENDING),
+            ("cc_members.username", pymongo.ASCENDING),
+        ]
+    )
+
+
+def create_allocations_indices(db):
+
+    db_collection = AllocationsRepository(database=db).get_collection()
+
+    # Index most useful for querying allocations for a given cluster
+    db_collection.create_index(
+        [
+            ("cluster_name", pymongo.ASCENDING),
+            ("start", pymongo.ASCENDING),
+            ("end", pymongo.ASCENDING),
+        ],
+    )
+
+    # Index most useful for querying allocations for any cluster
+    db_collection.create_index(
+        [
+            ("cluster_name", pymongo.ASCENDING),
+            ("start", pymongo.ASCENDING),
+            ("end", pymongo.ASCENDING),
+        ],
+    )
+
+
+def create_storages_indices(db):
+
+    db_collection = ClusterDiskUsageRepository(database=db).get_collection()
+
+    # Index most useful for querying diskusages for a given cluster and a given group
+    db_collection.create_index(
+        [
+            ("cluster_name", pymongo.ASCENDING),
+            ("groups.group_name", pymongo.ASCENDING),
+            ("timestamp", pymongo.ASCENDING),
+        ],
+    )
+
+    # Index most useful for querying diskusages for a given cluster and any group
+    db_collection.create_index(
+        [
+            ("cluster_name", pymongo.ASCENDING),
+            ("timestamp", pymongo.ASCENDING),
+        ],
+    )
+
+    # Index most useful for querying diskusages for any cluster and any group
+    db_collection.create_index(
+        [
+            ("timestamp", pymongo.ASCENDING),
+        ],
+    )
+
+
+def create_jobs_indices(db):
+
+    db_collection = SlurmJobRepository(database=db).get_collection()
+
+    # Index most useful for querying single jobs.
+    db_collection.create_index(
+        [
+            ("job_id", pymongo.ASCENDING),
+            ("cluster_name", pymongo.ASCENDING),
+            ("submit_time", pymongo.ASCENDING),
+        ],
+        unique=True,
+    )
+
+    # Index most useful for querying jobs for a given cluster and a given state
+    db_collection.create_index(
+        [
+            ("cluster_name", pymongo.ASCENDING),
+            ("job_state", pymongo.ASCENDING),
+            ("submit_time", pymongo.ASCENDING),
+            ("end_time", pymongo.ASCENDING),
+        ],
+    )
+
+    # Index most useful for querying jobs for a given cluster and any state
+    db_collection.create_index(
+        [
+            ("cluster_name", pymongo.ASCENDING),
+            ("submit_time", pymongo.ASCENDING),
+            ("end_time", pymongo.ASCENDING),
+        ],
+    )
+
+    # Index most useful for querying jobs on all clusters for a given state
+    db_collection.create_index(
+        [
+            ("job_state", pymongo.ASCENDING),
+            ("submit_time", pymongo.ASCENDING),
+            ("end_time", pymongo.ASCENDING),
+        ],
+    )
+
+    # Index most useful for querying jobs of any state on any cluster
+    db_collection.create_index(
+        [
+            ("submit_time", pymongo.ASCENDING),
+            ("end_time", pymongo.ASCENDING),
+        ],
+    )
