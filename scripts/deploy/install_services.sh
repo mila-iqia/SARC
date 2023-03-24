4b2e6fa0d2b6860d4914fb1f1ec5987bd521e019
Bruno Carrez <bruno.carrez@mila.quebec>
2023-03-24 14:00:34 -0400

    add systemd service to backup MongoDB each day

--- scripts/deploy/install_services.sh
+++ scripts/deploy/install_services.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
-#rm /etc/systemd/system/sarc_scrapers.service
-#rm /etc/systemd/system/sarc_scrapers.timer
 ln -s -f $SCRIPTPATH/../systemd/sarc_scrapers.service /etc/systemd/system/sarc_scrapers.service
 ln -s -f $SCRIPTPATH/../systemd/sarc_scrapers.timer /etc/systemd/system/sarc_scrapers.timer
+ln -s -f $SCRIPTPATH/../systemd/sarc_backup.service /etc/systemd/system/sarc_backup.service
+ln -s -f $SCRIPTPATH/../systemd/sarc_backup.timer /etc/systemd/system/sarc_backup.timer
 systemctl daemon-reload
 systemctl start sarc_scrapers.timer
 systemctl enable sarc_scrapers.timer
-
+systemctl start sarc_backup.timer
+systemctl enable sarc_backup.timer
---
+++ scripts/systemd/mongo_backup.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# This scripts makes a backup of the mongo database
+
+# usage :
+# ./mongo_backup.sh <container> <database> <dest_path>
+
+if [ -z "$1" ]
+  then
+    echo "No container name supplied, using default name 'sarc_mongo'"
+    CONTAINER='sarc_mongo'
+  else
+    CONTAINER=$1
+fi
+if [ -z "$2" ]
+  then
+    echo "No database name supplied, using default name 'sarc'"
+    DB='sarc'
+  else
+    DB=$2
+fi
+if [ -z "$3" ]
+  then
+    echo "No destination path supplied, using default path './'"
+    DEST_PATH='./'
+  else
+    DEST_PATH=$3
+fi
+echo "dump database $CONTAINER:$DB ..."
+podman exec $CONTAINER rm -rf temp_dump
+podman exec $CONTAINER mongodump -d $DB -o temp_dump --gzip
+
+FILENAME="$DEST_PATH$CONTAINER.$DB.$(date +"%Y-%m-%d")"
+echo "retrieve the db dump files to $FILENAME..."
+podman cp $CONTAINER:temp_dump $FILENAME
+podman exec $CONTAINER rm -rf temp_dump
