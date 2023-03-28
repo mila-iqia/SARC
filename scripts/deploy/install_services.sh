7a3ccf429ded94f99a87966b3233569b047a937b
Bruno Carrez <bruno.carrez@mila.quebec>
2023-03-27 23:18:15 -0400

    clean install script

--- scripts/deploy/install_services.sh
+++ scripts/deploy/install_services.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
-#rm /etc/systemd/system/sarc_scrapers.service
-#rm /etc/systemd/system/sarc_scrapers.timer
 ln -s -f $SCRIPTPATH/../systemd/sarc_scrapers.service /etc/systemd/system/sarc_scrapers.service
 ln -s -f $SCRIPTPATH/../systemd/sarc_scrapers.timer /etc/systemd/system/sarc_scrapers.timer
 ln -s -f $SCRIPTPATH/../systemd/sarc_containers.service /etc/systemd/system/sarc_containers.service
