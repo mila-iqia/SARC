8ae64789861d0b5433b85bceeaa9fa61dec6c7c3
Bruno Carrez <bruno.carrez@mila.quebec>
2023-03-30 16:06:44 -0400

    Mongo start on boot (#47)
    * systemd service to start containers at boot
    
    * update install documentation
    
    * clean install script
    
    * update doc

--- scripts/deploy/install_services.sh
+++ scripts/deploy/install_services.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
-#rm /etc/systemd/system/sarc_scrapers.service
-#rm /etc/systemd/system/sarc_scrapers.timer
 ln -s -f $SCRIPTPATH/../systemd/sarc_scrapers.service /etc/systemd/system/sarc_scrapers.service
 ln -s -f $SCRIPTPATH/../systemd/sarc_scrapers.timer /etc/systemd/system/sarc_scrapers.timer
+ln -s -f $SCRIPTPATH/../systemd/sarc_containers.service /etc/systemd/system/sarc_containers.service
 systemctl daemon-reload
 systemctl start sarc_scrapers.timer
 systemctl enable sarc_scrapers.timer
+systemctl start sarc_containers.service
+systemctl enable sarc_containers.service
 
