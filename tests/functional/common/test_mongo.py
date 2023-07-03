813b3e2322ad01c353b48e90a0f9aeb654504ef6
Pierre Delaunay <pierre@delaunay.io>
2023-07-03 11:48:52 -0400

    Test mongodb connection using configured uri

--- tests/functional/common/test_mongo.py
+++ tests/functional/common/test_mongo.py
@@ -1,15 +1,22 @@
 import pymongo
 
 
+from sarc.config import config
+
+
 # test connection to test mongo database
 def test_mongo_connection():
-    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    uri = config().mongo.connection_string
+
+    myclient = pymongo.MongoClient(uri)
     assert myclient != None
     assert myclient.list_database_names() != None
 
 
 def test_mongo_version():
-    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    uri = config().mongo.connection_string
+
+    myclient = pymongo.MongoClient(uri)
     assert myclient != None
     info = myclient.server_info()
     assert info["versionArray"][0] >= 6  # check if MongoDb >= v6.0
