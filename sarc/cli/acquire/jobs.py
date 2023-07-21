ccf77bf2da9a5b63b0b27b114526210378916508
Bruno Carrez <bruno.carrez@mila.quebec>
2023-07-21 02:26:51 -0400

    add the "auto" date parameter value for `acquire jobs` cli command (WIP) It will handle the automatic date selection from (last successful job scraping day + 1) until (today - 1) for a given cluster

--- sarc/cli/acquire/jobs.py
+++ sarc/cli/acquire/jobs.py
@@ -16,15 +16,20 @@ def _str_to_dt(dt_str: str) -> datetime:
     return datetime.strptime(dt_str, "%Y-%m-%d")
 
 
-def parse_dates(dates: list[str]) -> list[datetime]:
-    parsed_dates = []
+def parse_dates(dates: list[str], cluster_name: str) -> list[(datetime,bool)]:
+    parsed_dates = []  # return values are tuples (date, is_auto)
     for date in dates:
-        if date.count("-") == 5:
+        if date=="auto":
+            # TODO: remember this auto settings and construct dates from the last valid date+1 in the database
+            dates_auto = _dates_auto(cluster_name)
+            parsed_dates.extend([(date,True) for date in dates_auto])          
+            pass
+        elif date.count("-") == 5:
             start = _str_to_dt("-".join(date.split("-")[:3]))
             end = _str_to_dt("-".join(date.split("-")[3:]))
-            parsed_dates += list(_daterange(start, end))
+            parsed_dates.extend([(date,False) for date in _daterange(start, end)])  
         else:
-            parsed_dates.append(_str_to_dt(date))
+            parsed_dates.append((_str_to_dt(date),False))
 
     return parsed_dates
 
@@ -34,6 +39,19 @@ def _daterange(
 ) -> Generator[datetime, None, None]:
     for n in range(int((end_date - start_date).days)):
         yield start_date + timedelta(n)
+
+def _dates_auto(cluster_name: str) -> list[datetime]:
+    # we want to get the list of dates from the last valid date+1 in the database, until yesterday
+    dates = []
+    start = _dates_auto_first_date(cluster_name)
+    end = datetime.today()
+    return _daterange(start, end)
+
+def _dates_auto_first_date(cluster_name: str) -> datetime:
+    # get the last valid date in the database for the cluster
+    # TEMPORARY !!!!!!
+    return _str_to_dt("2023-07-01")
+
 
 
 @dataclass
@@ -54,12 +72,13 @@ class AcquireJobs:
         cfg = config()
         clusters_configs = cfg.clusters
 
-        for cluster_name, date in itertools.product(
-            self.cluster_names, parse_dates(self.dates)
-        ):
-            print(f"Acquire data on {cluster_name} for date: {date}")
-            sacct_mongodb_import(
-                clusters_configs[cluster_name], date, self.ignore_statistics
-            )
+        for cluster_name in self.cluster_names:
+            for date,is_auto in parse_dates(self.dates,cluster_name):
+                
+                print(f"Acquire data on {cluster_name} for date: {date} (is_auto={is_auto})")
+
+                sacct_mongodb_import(
+                    clusters_configs[cluster_name], date, self.ignore_statistics
+                )
 
         return 0
