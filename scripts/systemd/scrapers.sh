906c4f609cb4cdf438b9ca197e50ce1ce5e08b3a
Bruno Carrez <bruno.carrez@mila.quebec>
2023-07-24 15:08:33 -0400

    update scraping script to use automatic dates

--- scripts/systemd/scrapers.sh
+++ scripts/systemd/scrapers.sh
@@ -2,4 +2,4 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd $SCRIPTPATH/../../
-sudo -u sarc SARC_CONFIG=$SCRIPTPATH/../../config/sarc-prod.json ../.local/bin/poetry run sarc acquire jobs -c narval cedar beluga graham -d `date -d "yesterday" +%Y-%m-%d`
+sudo -u sarc SARC_CONFIG=$SCRIPTPATH/../../config/sarc-prod.json ../.local/bin/poetry run sarc acquire jobs -c narval cedar beluga graham -d auto
