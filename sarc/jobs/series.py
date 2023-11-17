a2979c4022455a3ac672e7c3fd609529a6419dc3
notoraptor <stevenbocco@gmail.com>
2023-11-17 13:38:46 -0500

    Add old code for load_job_series

--- sarc/jobs/series.py
+++ sarc/jobs/series.py
@@ -1,13 +1,19 @@
 from __future__ import annotations
 
+from datetime import timedelta
+from sarc.jobs import get_jobs
+import os
+import pandas as pd
 from datetime import datetime
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Callable
 
 import pandas
 from pandas import DataFrame
 from prometheus_api_client import MetricRangeDataFrame
+import time
+from tqdm import tqdm
 
-from sarc.config import MTL, UTC
+from sarc.config import MTL, UTC, config
 from sarc.jobs.job import JobStatistics, Statistics
 
 if TYPE_CHECKING:
@@ -222,15 +228,15 @@ def load_job_series(
         *,  # All arguments from `get_jobs`
         fields: None | list[str] | dict[str, str] = None,
         clip_time: bool = False,
-        callback: None | Callable = None):
+        callback: None | Callable = None, **jobs_args):
     """
     Query jobs from the database and return them in a DataFrame, including full user info
-    for each jobs.
+    for each job.
 
     Parameters
     ----------
     fields: list or dict
-        Job fields to include in the DataFrame. By default include all fields.
+        Job fields to include in the DataFrame. By default, include all fields.
         A dictionary may be passed to select fields and rename them in the DataFrame.
         In such case, the keys are the fields' names and the values are the names
         they will have in the DataFrame.
@@ -239,7 +245,115 @@ def load_job_series(
         Defaults to False.
     callback: Callable
         Callable taking the current job dictionary in the format it would be included in the DataFrame.
+    jobs_args
+        Arguments to be passed to `get_jobs` to query jobs from the database.
     """
+
+
+def load_job_series_old(start, end, filename=None, checkpoint_interval=60) -> pd.DataFrame:
+    if filename and os.path.exists(filename):
+        return pd.read_pickle(filename)
+
+    total = config().mongo.database_instance.jobs.count_documents(
+        {
+            "$or": [
+                {"submit_time": {"$lt": end}, "end_time": None},
+                {"submit_time": {"$lt": end}, "end_time": {"$gt": start}},
+            ]
+        }
+    )
+
+    checkpoint = time.time()
+
+    rows = []
+    # Fetch all jobs from the clusters
+    for job in tqdm(get_jobs(start=start, end=end), total=total):
+        # if job.elapsed_time <= 0:
+        #     continue
+
+        if job.end_time is None:
+            job.end_time = datetime.now(tz=MTL)
+
+        # For some reason start time is not reliable, often equal to submit time,
+        # so we infer it based on end_time and elapsed_time.
+        job.start_time = job.end_time - timedelta(seconds=job.elapsed_time)
+
+        # Clip the job to the time range we are interested in.
+        # NOTE: This should perhaps be helper function for dataframes so that we don't force clipping raw data
+        #       during loading.
+        unclipped_start = job.start_time
+        if job.start_time < start:
+            job.start_time = start
+        unclipped_end = job.end_time
+        if job.end_time > end:
+            job.end_time = end
+        # Could be negative if job started after end. We don't want to filter
+        # them out because they have been submitted before end and we want to
+        # compute their wait time.
+        job.elapsed_time = max((job.end_time - job.start_time).total_seconds(), 0)
+
+        # We only care about jobs that actually ran.
+        # if job.elapsed_time <= 0:
+        #     continue
+
+        is_on_mig = job.cluster_name == "mila" and bool(NODE_WITH_MIG & set(job.nodes))
+
+        no_stats = False
+        # If Job is on a node with MIG GPUs, ignore.
+        if is_on_mig or job.stored_statistics is None:
+            no_stats = True
+
+        if no_stats:
+            job_series = copy.deepcopy(DUMMY_STATS)
+        else:
+            job_series = job.stored_statistics.dict(include=SERIES_INCLUDE)
+            job_series = {
+                k: v["median"] if v else np.nan for k, v in job_series.items()
+            }
+
+        # TODO: Verify that all stats are set to -1 when GPU was MIG.
+
+        # TODO: Why is it possible to have billing smaller than gres_gpu???
+        billing = job.allocated.billing or 0
+        gres_gpu = job.allocated.gres_gpu or 0
+        if gres_gpu:
+            job_series["gpu_allocated"] = max(billing, gres_gpu)
+            job_series["cpu"] = job.allocated.cpu
+        else:
+            job_series["gpu_allocated"] = 0
+            job_series["cpu"] = (
+                max(billing, job.allocated.cpu) if job.allocated.cpu else 0
+            )
+        job_series["gpu_requested"] = gres_gpu
+        job_series["duration"] = job.elapsed_time
+        job_series["mem"] = job.allocated.mem
+        job_series["mig"] = bool(is_on_mig)
+        job_series["submit"] = job.submit_time
+        job_series["start"] = job.start_time
+        job_series["end"] = job.end_time
+        job_series["unclipped_start"] = unclipped_start
+        job_series["unclipped_end"] = unclipped_end
+        job_series["constraints"] = job.constraints
+
+        info = job.dict(include=JOB_INCLUDE)
+        for key in info:
+            job_series[key] = info[key]
+
+        rows.append(job_series)
+
+        if filename and (time.time() - checkpoint) > checkpoint_interval:
+            df = pd.DataFrame(rows)
+            df.to_pickle(filename)
+            checkpoint = time.time()
+
+    df = pd.DataFrame(rows)
+
+    if filename:
+        df.to_pickle(filename)
+
+    assert isinstance(df, pd.DataFrame)
+
+    return df
 
 
 slurm_job_metric_names = [
