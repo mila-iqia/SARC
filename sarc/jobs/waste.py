539a36afb3dd549e83d01a8d6bdd6fded0636c51
notoraptor <stevenbocco@gmail.com>
2023-12-04 15:57:13 -0500

    Update waste code and compute all fields.

--- sarc/jobs/waste.py
+++ sarc/jobs/waste.py
@@ -1,25 +1,26 @@
 def compute_cost_and_waste(full_df):
-    cpu_jobs = get_cpu_jobs(full_df)
-    gpu_jobs = get_gpu_jobs(full_df)
-
-    def compute_device_cost(data, device):
-        return data["duration"] * data[device]
-
-    def compute_wastes(data, device):
-        data[f"{device}_cost"] = compute_device_cost(data, device)
-        data[f"{device}_waste"] = (1 - data[f"{device}_utilization"]) * data[f"{device}_cost"]
-        # TODO: Add device-equivalent if available.
-        return data
-
-    full_df = compute_wastes(full_df, "cpu")
-    full_df = compute_wastes(full_df, "gpu")
-
+    full_df = _compute_cost_and_wastes(full_df, "cpu")
+    full_df = _compute_cost_and_wastes(full_df, "gpu")
     return full_df
 
 
-def get_cpu_jobs(data):
-    return data[data["gpu_requested"] == 0]
+def _compute_cost_and_wastes(data, device):
+    device_col = {"cpu": "cpu", "gpu": "gres_gpu"}[device]
 
+    data[f"{device}_cost"] = data["elapsed_time"] * data[f"requested.{device_col}"]
+    data[f"{device}_waste"] = (1 - data[f"{device}_utilization"]) * data[
+        f"{device}_cost"
+    ]
 
-def get_gpu_jobs(data):
-    return data[data["gpu_requested"] > 0]
+    data[f"{device}_equivalent_cost"] = (
+        data["elapsed_time"] * data[f"allocated.{device_col}"]
+    )
+    data[f"{device}_equivalent_waste"] = (1 - data[f"{device}_utilization"]) * data[
+        f"{device}_equivalent_cost"
+    ]
+
+    data[f"{device}_overbilling_cost"] = data["elapsed_time"] * (
+        data[f"allocated.{device_col}"] - data[f"requested.{device_col}"]
+    )
+
+    return data
