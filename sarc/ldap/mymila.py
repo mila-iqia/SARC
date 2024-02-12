6685ba12d7df243d3a19f14b5ecf547f393a9e61
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2024-02-12 09:25:42 -0500

    black again

--- sarc/ldap/mymila.py
+++ sarc/ldap/mymila.py
@@ -13,4 +13,6 @@ def query_mymila(cfg: MyMilaConfig):
     #       because the mocked config is on `open`. It stinks, but we
     #       will replace this part of the code in favor of direct calls to
     #       MyMila API anyway.
-    return pd.DataFrame(json.loads(open(cfg.tmp_json_path, "r", encoding='uft-8').read()))
+    return pd.DataFrame(
+        json.loads(open(cfg.tmp_json_path, "r", encoding="uft-8").read())
+    )
