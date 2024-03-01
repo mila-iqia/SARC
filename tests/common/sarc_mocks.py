be44d1a5f2ee0ed5bac764009e8d3ed87518cd2a
soline-b <85968577+soline-b@users.noreply.github.com>
2024-03-01 00:53:01 -0500

    Test supervisor and co-supervisor import (#104)
    * Replace empty values by NA (from pandas) in the users data retrieved from the LDAP and MyMila
    
    * Add an option to override mocked data in the tests
    
    * Add test to check if the supervisor has correctly been retrieved from MyMila and LDAP data
    
    * Add test to check the update of the supervisor from users data from the LDAP and MyMila
    
    * Black reformatting
    
    * Determine the supervisor or co_supervisor by its name
    
    * Tox reformatting
    
    * fix students and profs mocked affiliation types swapped by mistake
    
    ---------
    
    Co-authored-by: Bruno Carrez <bruno.carrez@mila.quebec>

--- tests/common/sarc_mocks.py
+++ tests/common/sarc_mocks.py
@@ -17,14 +17,6 @@ _membership_types = [
     ],
 ]
 _affiliation_types = [
-    # Prof
-    [
-        "Collaborating Alumni",
-        "Collaborating researcher",
-        "HQP - Postdoctorate",
-        "visiting researcher",
-        "",
-    ],
     # Student
     [
         "HQP - DESS",
@@ -33,6 +25,14 @@ _affiliation_types = [
         "HQP - Professional Master's",
         "HQP - Undergraduate",
         "Research Intern",
+        "",
+    ],
+    # Prof
+    [
+        "Collaborating Alumni",
+        "Collaborating researcher",
+        "HQP - Postdoctorate",
+        "visiting researcher",
         "",
     ],
 ]
@@ -118,8 +118,8 @@ def dictset(dictionnary: dict, operation: dict):
     return result
 
 
-def fake_mymila_data(nbr_users=10, nbr_profs=5):
-    entry_ctor = mymila_entry_builder(nbr_profs)
+def fake_mymila_data(nbr_users=10, nbr_profs=5, hardcoded_values_by_user={}):
+    entry_ctor = mymila_entry_builder(nbr_profs, hardcoded_values_by_user)
 
     return pd.DataFrame(list([entry_ctor(i) for i in range(nbr_users)]))
 
@@ -128,7 +128,7 @@ def fake_mymila_data_with_history(nbr_users=10, nbr_profs=5):
     pass
 
 
-def mymila_entry_builder(nbr_profs=5):
+def mymila_entry_builder(nbr_profs=5, hardcoded_values_by_user={}):
     """
     Return a deterministically-generated list of fake MyMila users just as
     they would be returned by the function `load_mymila` (yet to be developped).
@@ -181,32 +181,82 @@ def mymila_entry_builder(nbr_profs=5):
         last_name = f"Smith{i:03d}"
         email = f"john.smith{i:03d}@mila.quebec"
 
+        def _define_value(i, key, default_value):
+            """
+            Retrieve the hardcoded value to associate to a key for a user, if any,
+            or use the default value given as parameter.
+
+            Parameters:
+                i               Index of the user in the list we want to generate
+                key             The key of the element we want to check if there is a hardcoded value for
+                default_value   The value to associate to the user and key if no hardcoded value
+                                is associated to it
+            Returns:
+                The hardcoded value defined for the user, if any.
+                The default value given as parameter otherwise.
+            """
+            return define_value(i, key, default_value, hardcoded_values_by_user)
+
         return dictset(
             mymila_template,
             {
-                "Status": status[i % len(status)],
-                "Last Name": first_name,
-                "First Name": last_name,
-                "Preferred First Name": first_name,
-                "MILA Email": email,
-                "Start Date with MILA": date(2022, 1, 1),
-                "End Date with MILA": [date(2027, 12, 31), None][i % 2],
-                "Supervisor Principal": supervisors[i % len(supervisors)],
-                "Co-Supervisor": supervisors[(i + 1) % len(supervisors)],
+                "Status": _define_value(i, "Status", status[i % len(status)]),
+                "Last Name": _define_value(i, "Last Name", last_name),
+                "First Name": _define_value(i, "First Name", first_name),
+                "Preferred First Name": _define_value(
+                    i, "Preferred First Name", first_name
+                ),
+                "MILA Email": _define_value(i, "MILA Email", email),
+                "Start Date with MILA": _define_value(
+                    i, "Start Date with MILA", date(2022, 1, 1)
+                ),
+                "End Date with MILA": _define_value(
+                    i, "End Date with MILA", [date(2027, 12, 31), None][i % 2]
+                ),
+                "Supervisor Principal": _define_value(
+                    i, "Supervisor Principal", supervisors[i % len(supervisors)]
+                ),
+                "Co-Supervisor": _define_value(
+                    i, "Co-Supervisor", supervisors[(i + 1) % len(supervisors)]
+                ),
                 # Optional
-                "Membership Type": membership_types[i % len(membership_types)],
-                "Affiliation type": affiliation_types[i % len(affiliation_types)],
-                "Faculty affiliated": faculty_affiliated[i % len(faculty_affiliated)],
-                "Program of study": program_of_study[i % len(program_of_study)],
-                "Start date of studies": date(year=2022, month=1, day=1),
-                "End date of studies": date(year=2027, month=12, day=31),
-                "Affiliated university": affiliated_university[
-                    i % len(affiliated_university)
-                ],
-                "Current university title": uni_title,
-                "Start date of academic nomination": date(2022, 1, 1),
-                "End date of academic nomination": [date(2027, 12, 31), None][i % 2],
-                "Email": email,
+                "Membership Type": _define_value(
+                    i, "Membership Type", membership_types[i % len(membership_types)]
+                ),
+                "Affiliation type": _define_value(
+                    i, "Affiliation type", affiliation_types[i % len(affiliation_types)]
+                ),
+                "Faculty affiliated": _define_value(
+                    i,
+                    "Faculty affiliated",
+                    faculty_affiliated[i % len(faculty_affiliated)],
+                ),
+                "Program of study": _define_value(
+                    i, "Program of study", program_of_study[i % len(program_of_study)]
+                ),
+                "Start date of studies": _define_value(
+                    i, "Start date of studies", date(year=2022, month=1, day=1)
+                ),
+                "End date of studies": _define_value(
+                    i, "End date of studies", date(year=2027, month=12, day=31)
+                ),
+                "Affiliated university": _define_value(
+                    i,
+                    "Affiliated university",
+                    affiliated_university[i % len(affiliated_university)],
+                ),
+                "Current university title": _define_value(
+                    i, "Current university title", uni_title
+                ),
+                "Start date of academic nomination": _define_value(
+                    i, "Start date of academic nomination", date(2022, 1, 1)
+                ),
+                "End date of academic nomination": _define_value(
+                    i,
+                    "End date of academic nomination",
+                    [date(2027, 12, 31), None][i % 2],
+                ),
+                "Email": _define_value(i, "Email", email),
             },
         )
 
@@ -228,44 +278,92 @@ def fake_member_of(index, count):
     return member_of_config.get(index, [])
 
 
-def fake_raw_ldap_data(nbr_users=10):
+def define_value(i, key, default_value, hardcoded_values_by_user):
+    """ """
+
+    return (
+        hardcoded_values_by_user[i][key]
+        if i in hardcoded_values_by_user and key in hardcoded_values_by_user[i]
+        else default_value
+    )
+
+
+def fake_raw_ldap_data(nbr_users=10, hardcoded_values_by_user={}):
     """
     Return a deterministically-generated list of fake LDAP users just as
     they would be returned by the function `query_ldap`.
     This is used for mocking the LDAP server.
+
+    Parameters:
+        nbr_users                   The number of users we want to generate
+        hardcoded_values_by_user    Dictionary associating the index of a user to
+                                    a dictionary of the values we want to hardcode
     """
+
+    def _define_value(i, key, default_value):
+        """
+        Retrieve the hardcoded value to associate to a key for a user, if any,
+        or use the default value given as parameter.
+
+        Parameters:
+            i               Index of the user in the list we want to generate
+            key             The key of the element we want to check if there is a hardcoded value for
+            default_value   The value to associate to the user and key if no hardcoded value
+                            is associated to it
+        Returns:
+            The hardcoded value defined for the user, if any.
+            The default value given as parameter otherwise.
+        """
+        return define_value(i, key, default_value, hardcoded_values_by_user)
+
     return list(
         [
             {
-                "apple-generateduid": ["AF54098F-29AE-990A-B1AC-F63F5A89B89"],
-                "cn": [f"john.smith{i:03d}", f"John Smith{i:03d}"],
-                "departmentNumber": [],
-                "displayName": [f"John Smith the {i:03d}rd"],
-                "employeeNumber": [],
-                "employeeType": [],
-                "gecos": [""],
-                "gidNumber": [str(1500000001 + i)],
-                "givenName": ["John"],
-                "googleUid": [f"john.smith{i:03d}"],
-                "homeDirectory": [f"/home/john.smith{i:03d}"],
-                "loginShell": ["/bin/bash"],
-                "mail": [f"john.smith{i:03d}@mila.quebec"],
-                "memberOf": fake_member_of(i, nbr_users),
-                "objectClass": [
-                    "top",
-                    "person",
-                    "organizationalPerson",
-                    "inetOrgPerson",
-                    "posixAccount",
-                ],
-                "physicalDeliveryOfficeName": [],
-                "posixUid": [f"smithj{i:03d}"],
-                "sn": [f"Smith {i:03d}"],
-                "suspended": ["false"],
-                "telephoneNumber": [],
-                "title": [],
-                "uid": [f"john.smith{i:03d}"],
-                "uidNumber": [str(1500000001 + i)],
+                "apple-generateduid": _define_value(
+                    i, "apple-generateduid", ["AF54098F-29AE-990A-B1AC-F63F5A89B89"]
+                ),
+                "cn": _define_value(
+                    i, "cn", [f"john.smith{i:03d}", f"John Smith{i:03d}"]
+                ),
+                "departmentNumber": _define_value(i, "departmentNumber", []),
+                "displayName": _define_value(
+                    i, "displayName", [f"John Smith the {i:03d}rd"]
+                ),
+                "employeeNumber": _define_value(i, "employeeNumber", []),
+                "employeeType": _define_value(i, "employeeType", []),
+                "gecos": _define_value(i, "gecos", [""]),
+                "gidNumber": _define_value(i, "gidNumber", [str(1500000001 + i)]),
+                "givenName": _define_value(i, "givenName", ["John"]),
+                "googleUid": _define_value(i, "googleUid", [f"john.smith{i:03d}"]),
+                "homeDirectory": _define_value(
+                    i, "homeDirectory", [f"/home/john.smith{i:03d}"]
+                ),
+                "loginShell": _define_value(i, "loginShell", ["/bin/bash"]),
+                "mail": _define_value(i, "mail", [f"john.smith{i:03d}@mila.quebec"]),
+                "memberOf": _define_value(i, "memberOf", fake_member_of(i, nbr_users)),
+                "objectClass": _define_value(
+                    i,
+                    "objectClass",
+                    [
+                        "top",
+                        "person",
+                        "organizationalPerson",
+                        "inetOrgPerson",
+                        "posixAccount",
+                    ],
+                ),
+                "physicalDeliveryOfficeName": _define_value(
+                    i, "physicalDeliveryOfficeName", []
+                ),
+                "posixUid": _define_value(i, "posixUid", [f"smithj{i:03d}"]),
+                "sn": _define_value(i, "sn", [f"Smith {i:03d}"]),
+                "suspended": _define_value(i, "suspended", ["false"]),
+                "telephoneNumber": _define_value(i, "telephoneNumber", []),
+                "title": _define_value(i, "title", []),
+                "uid": _define_value(i, "uid", [f"john.smith{i:03d}"]),
+                "uidNumber": _define_value(i, "uidNumber", [str(1500000001 + i)]),
+                "supervisor": _define_value(i, "supervisor", None),
+                "co_supervisor": _define_value(i, "co_supervisor", None),
             }
             for i in range(nbr_users)
         ]
