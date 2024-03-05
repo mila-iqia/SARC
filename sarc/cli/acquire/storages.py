7bd0466d81aa8101c56501aec4fdab0af8a99f04
Bruno Carrez <bruno.carrez@mila.quebec>
2024-03-05 00:58:11 -0500

    add logs and trace in acquire diskusage

--- sarc/cli/acquire/storages.py
+++ sarc/cli/acquire/storages.py
@@ -1,5 +1,7 @@
+import logging
 from dataclasses import dataclass
 
+from opentelemetry.trace import Status, StatusCode, get_tracer
 from simple_parsing import field
 
 from sarc.config import config
@@ -18,22 +20,29 @@ class AcquireStorages:
     cluster_names: list[str] = field(alias=["-c"], default_factory=list)
     dry: bool = False
 
+    tracer = get_tracer("AcquireStorages")
+
     def execute(self) -> int:
         cfg = config()
 
         for cluster_name in self.cluster_names:
-            print(f"Acquiring {cluster_name} storages report...")
+            with self.tracer.start_as_current_span("cluster") as span:
+                span.set_attribute("cluster_name", cluster_name)
 
-            cluster = cfg.clusters[cluster_name]
+                logging.info(f"Acquiring {cluster_name} storages report...")
 
-            fetch_diskusage = methods.get(cluster_name, methods["default"])
-            du = fetch_diskusage(cluster)
+                cluster = cfg.clusters[cluster_name]
 
-            if not self.dry:
-                collection = get_diskusage_collection()
-                collection.add(du)
-            else:
-                print("Document:")
-                print(du.json(indent=2))
+                fetch_diskusage = methods.get(cluster_name, methods["default"])
+                du = fetch_diskusage(cluster)
+
+                if not self.dry:
+                    collection = get_diskusage_collection()
+                    collection.add(du)
+                else:
+                    print("Document:")
+                    print(du.json(indent=2))
+
+                span.set_status(Status(StatusCode.OK))
 
         return 0
