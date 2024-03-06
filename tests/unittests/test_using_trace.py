ccff9227547e02fc263942e09c008ff467df4090
Bruno Carrez <bruno.carrez@mila.quebec>
2024-03-06 14:25:36 -0500

    tests for nested traces

--- tests/unittests/test_using_trace.py
+++ tests/unittests/test_using_trace.py
@@ -80,3 +80,49 @@ def test_using_trace_unexpected_exception(captrace):
     assert len(traces) == 2
     assert traces[0].status.status_code == StatusCode.OK
     assert traces[1].status.status_code == StatusCode.ERROR
+
+
+def test_using_trace_noerror_nested_1_tracer(captrace):
+    # test nested spans in the same tracer
+    with using_trace("test_using_trace_nested_spans", "span1") as span:
+        span.add_event("event1.1")
+        with using_trace("test_using_trace_nested_spans", "span2") as span2:
+            span2.add_event("event2")
+        span.add_event("event1.2")
+
+    # check traces
+    traces = captrace.get_finished_spans()
+    print(f"traces: {traces}")
+    assert len(traces) == 2
+
+    assert traces[0].name == "span2"
+    assert len(traces[0].events) == 1
+    assert traces[0].events[0].name == "event2"
+
+    assert traces[1].name == "span1"
+    assert len(traces[1].events) == 2
+    assert "event1.1" in [e.name for e in traces[1].events]
+    assert "event1.2" in [e.name for e in traces[1].events]
+
+
+def test_using_trace_noerror_nested_2_tracers(captrace):
+    # test netsed spans in different tracers
+    with using_trace("test_using_trace_nested_trace_1", "span1") as span:
+        span.add_event("event1.1")
+        with using_trace("test_using_trace_nested_trace_2", "span2") as span2:
+            span2.add_event("event2")
+        span.add_event("event1.2")
+
+    # check traces
+    traces = captrace.get_finished_spans()
+    print(f"traces: {traces}")
+    assert len(traces) == 2
+
+    assert traces[0].name == "span2"
+    assert len(traces[0].events) == 1
+    assert traces[0].events[0].name == "event2"
+
+    assert traces[1].name == "span1"
+    assert len(traces[1].events) == 2
+    assert "event1.1" in [e.name for e in traces[1].events]
+    assert "event1.2" in [e.name for e in traces[1].events]
