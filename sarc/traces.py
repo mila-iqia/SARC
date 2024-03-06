c4a3811d5f299b773f413106a9db663b001a4e68
Bruno Carrez <bruno.carrez@mila.quebec>
2024-03-06 13:42:09 -0500

    update docstring of `using_trace`

--- sarc/traces.py
+++ sarc/traces.py
@@ -7,16 +7,23 @@ from opentelemetry.trace import Status, StatusCode, get_tracer
 # and catch exceptions (in span) without breaking the whole program execution
 @contextmanager
 def using_trace(
-    tracer_name: str, span_name: str, exception_types=[Exception]
+    tracer_name: str, span_name: str, exception_types: list = [Exception]
 ):  # pylint: disable=dangerous-default-value
     """
-    Context manager to manage traces & span,
-    and catch exceptions (in span) without breaking the whole program execution.
-        Parameters:
-                tracer_name (str): name of the tracer (can be shared between different spans)
-                span_name (str): name of the span
-                exception_types ([type,..]): types of exceptions to catch, other types will be raised
-                    by default, `Exception` is caught so that nothing would be raised
+    Context manager to manage traces & span, and catch exceptions (in span) without breaking the whole program execution.
+    Parameters
+    ----------
+    tracer_name : str
+        name of the tracer (can be shared between different spans)
+    span_name : str
+        name of the span
+    exception_types : list
+        Types of exceptions to catch, other types will be raised.
+        By default, `Exception` is caught so that nothing would be raised.
+    Yields
+    ------
+    span : opentelemetry.trace.Span
+        The span created by the context manager.
     """
     tracer = get_tracer(tracer_name)
     with tracer.start_as_current_span(span_name) as span:
