cb788cbe6bfffdc4086e9dfa8144a7d4d6089c08
Bruno Carrez <bruno.carrez@mila.quebec>
2024-03-06 14:29:43 -0500

    update

--- tests/unittests/test_using_trace.py
+++ tests/unittests/test_using_trace.py
@@ -41,22 +41,16 @@ def test_using_trace_default_exception(captrace):
 
 def test_using_trace_expected_exception(captrace):
     exception_caught = False
-    try:
-        with using_trace("test_using_trace_noerror", "span1") as span:
-            pass
-        with using_trace(
-            "test_using_trace_default_exception",
-            "span2",
-            exception_types=[ZeroDivisionError],
-        ) as span:
-            span.add_event("event1")
-            a = 1 / 0
-    except Exception:
-        exception_caught = True
+    with using_trace("test_using_trace_noerror", "span1") as span:
+        pass
+    with using_trace(
+        "test_using_trace_default_exception",
+        "span2",
+        exception_types=[ZeroDivisionError],
+    ) as span:
+        span.add_event("event1")
+        a = 1 / 0
 
-    assert (
-        exception_caught == False
-    )  # the exception should be handled by the context manager
     traces = captrace.get_finished_spans()
     assert len(traces) == 2
     assert traces[0].status.status_code == StatusCode.OK
@@ -95,7 +89,9 @@ def test_using_trace_noerror_nested_1_tracer(captrace):
     print(f"traces: {traces}")
     assert len(traces) == 2
 
-    assert traces[0].name == "span2"
+    assert (
+        traces[0].name == "span2"
+    )  # spans are in their order of ending, so span2 is first
     assert len(traces[0].events) == 1
     assert traces[0].events[0].name == "event2"
 
@@ -118,7 +114,9 @@ def test_using_trace_noerror_nested_2_tracers(captrace):
     print(f"traces: {traces}")
     assert len(traces) == 2
 
-    assert traces[0].name == "span2"
+    assert (
+        traces[0].name == "span2"
+    )  # spans are in their order of ending, so span2 is first
     assert len(traces[0].events) == 1
     assert traces[0].events[0].name == "event2"
 
@@ -126,3 +124,36 @@ def test_using_trace_noerror_nested_2_tracers(captrace):
     assert len(traces[1].events) == 2
     assert "event1.1" in [e.name for e in traces[1].events]
     assert "event1.2" in [e.name for e in traces[1].events]
+
+
+def test_using_trace_error_nested(captrace):
+    # test nested spans in the same tracer
+    with using_trace("test_using_trace_nested_spans", "span1") as span:
+        span.add_event("event1.1")
+        with using_trace(
+            "test_using_trace_nested_spans",
+            "span2",
+            exception_types=[ZeroDivisionError],
+        ) as span2:
+            span2.add_event("event2")
+            a = 1 / 0
+        span.add_event("event1.2")
+
+    # check traces
+    traces = captrace.get_finished_spans()
+    print(f"traces: {traces}")
+    assert len(traces) == 2
+
+    assert (
+        traces[0].name == "span2"
+    )  # spans are in their order of ending, so span2 is first
+    assert len(traces[0].events) == 2
+    assert traces[0].events[0].name == "event2"
+    assert traces[0].events[1].name == "exception"
+    assert traces[0].status.status_code == StatusCode.ERROR
+
+    assert traces[1].name == "span1"
+    assert len(traces[1].events) == 2
+    assert "event1.1" in [e.name for e in traces[1].events]
+    assert "event1.2" in [e.name for e in traces[1].events]
+    assert traces[1].status.status_code == StatusCode.OK
