6565e33beb8b11efd040d2e3c73da224015feea3
Olivier Breuleux <breuleux@gmail.com>
2024-03-13 14:09:19 -0400

    Add latest_job_series

--- sarc/alerts/fixtures.py
+++ sarc/alerts/fixtures.py
@@ -1,6 +1,7 @@
 import logging
 
 from sarc.jobs.job import get_jobs
+from sarc.jobs.series import load_job_series
 
 from .cache import Timespan, spancache
 
@@ -13,3 +14,10 @@ def latest_jobs(timespan: Timespan):
     start, end = timespan.calculate_bounds()
     jobs = get_jobs(start=start, end=end)
     return list(jobs)
+
+
+@spancache
+def latest_job_series(timespan: Timespan):
+    logger.info(f"Querying job series from the last {timespan}...")
+    start, end = timespan.calculate_bounds()
+    return load_job_series(start=start, end=end)
