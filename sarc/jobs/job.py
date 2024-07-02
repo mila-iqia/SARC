842c6cf87d916dbe83f9bde18fd8518d037cd2a4
notoraptor <stevenbocco@gmail.com>
2024-07-02 08:44:44 -0400

    Add a todo

--- sarc/jobs/job.py
+++ sarc/jobs/job.py
@@ -230,5 +230,6 @@ def jobs_collection():
 @cache
 def get_clusters():
     """Fetch all possible clusters"""
+    # TODO Is this function still useful ? Currently used only in sarc.cli.utils
     jobs = jobs_collection().get_collection()
     return jobs.distinct("cluster_name", {})
