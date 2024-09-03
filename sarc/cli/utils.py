ed1bb2d4b86f711ad63ab4f72752b78db37d1c4e
notoraptor <stevenbocco@gmail.com>
2024-09-03 11:25:58 -0400

    Move old specific function get_clusters() into sarc.cli.utils
    NB:
    - This function is used only here.
    - This function lists only clusters that appear in available jobs. If jobs never ran in a cluster, it won't be listed by this function.
      So, to get all available clusters, instead use sarc.client.job.get_available_clusters(), which instead list clusters from db clusters collection.

--- sarc/cli/utils.py
+++ sarc/cli/utils.py
@@ -1,4 +1,16 @@
-from sarc.jobs.job import get_clusters
+from __future__ import annotations
+
+from functools import cache
+
+from sarc.client.job import _jobs_collection
+
+
+@cache
+def get_clusters():
+    """Fetch all possible clusters"""
+    # NB: Is this function still useful ? Currently used only in sarc.cli.utils
+    jobs = _jobs_collection().get_collection()
+    return jobs.distinct("cluster_name", {})
 
 
 class ChoicesContainer:
