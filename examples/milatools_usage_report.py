baabd19bc2abe9458b71870c3c03469d56dfff15
Fabrice Normandin <normandf@mila.quebec>
2024-09-05 17:11:53 -0400

    Annotate the current usage percent
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- examples/milatools_usage_report.py
+++ examples/milatools_usage_report.py
@@ -13,7 +13,6 @@
 from __future__ import annotations
 
 import dataclasses
-import hashlib
 import logging
 import os
 import pickle
@@ -33,14 +32,13 @@ import pandas as pd
 import pymongo
 import pymongo.collection
 import simple_parsing
+from pandas.core.indexes.datetimes import DatetimeIndex
 from typing_extensions import TypeGuard
 
 from sarc.config import MTL
 from sarc.jobs.job import (
     Query,
-    SlurmJob,
     _compute_jobs_query,
-    get_jobs,
     jobs_collection,
 )
 
@@ -111,11 +109,13 @@ def main():
     all_clusters = _get_all_clusters(args.start_date, args.end_date)
     logger.info(f"All clusters: {all_clusters}")
 
-    figures = make_milatools_usage_plots(args, cluster="mila", fig_suffix="mila")
-    figures = make_milatools_usage_plots(
-        args, cluster=list(set(all_clusters) - {"mila"}), fig_suffix="drac"
+    figures: list[Path] = []
+
+    figures += make_milatools_usage_plots(period, cluster="mila", fig_suffix="mila")
+    figures += make_milatools_usage_plots(
+        period, cluster=sorted(set(all_clusters) - {"mila"}), fig_suffix="drac"
     )
-    figures = make_milatools_usage_plots(args, cluster=None, fig_suffix="all")
+    figures += make_milatools_usage_plots(period, cluster=None, fig_suffix="all")
     # figures = make_usage_plots(args, job_name="mila-code")
     # figures += make_usage_plots(args, job_name="mila-cpu")
 
@@ -135,7 +135,7 @@ def _get_all_clusters(start_date: datetime, end_date: datetime):
     ).exists():
         with all_clusters_file.open("rb") as f:
             all_clusters = pickle.load(f)
-        assert is_iterable_of(all_clusters, str) and isinstance(all_clusters, list)
+        assert _is_iterable_of(all_clusters, str) and isinstance(all_clusters, list)
     else:
         _period_filter = _compute_jobs_query(start=start_date, end=end_date)
         all_clusters: list[str] = list(
@@ -143,19 +143,23 @@ def _get_all_clusters(start_date: datetime, end_date: datetime):
         )
         with all_clusters_file.open("wb") as f:
             pickle.dump(all_clusters, f)
-    return all_clusters
+    return sorted(all_clusters)
 
 
 def make_milatools_usage_plots(
     period: Period, cluster: str | list[str] | None, fig_suffix: str
 ) -> list[Path]:
-    df = get_milatools_usage_data(period, cluster=cluster)
+    if cluster is None:
+        cluster_suffix = " on all slurm clusters"
+    elif isinstance(cluster, str):
+        cluster_suffix = f" on the {cluster} cluster"
+    else:
+        cluster_suffix = f" on the {cluster} clusters"
+
+    df = _get_milatools_usage_data(period, cluster=cluster)
     df["using_milatools"] = (
         df["milatools_users_this_period"] / df["cluster_users_this_period"]
     )
-    # df["percentage_used_before"] = (
-    #     df["milatools_users_so_far"] / df["cluster_users_so_far"]
-    # )
     df["used_milatools_before"] = (
         df["users_this_period_that_used_milatools_before"]
         / df["cluster_users_this_period"]
@@ -165,67 +169,60 @@ def make_milatools_usage_plots(
     # daily_counts = df.resample(rule="D").size()
     axes: list[matplotlib.axes.Axes]
     fig, axes = plt.subplots(sharex=True, ncols=2, nrows=1)
+    fig.suptitle(f"Statistics on the use of Milatools{cluster_suffix}")
     (ax1, ax2) = axes
+    ax1.set_title("Percentage of users using milatools")
+    ax1.set_ylim(0, 1)
+    ax1.annotate(
+        f'{df["using_milatools"].iloc[-1]:.2%}',
+        (df.index[-1], df["using_milatools"].iloc[-1]),
+    )
+    ax1.annotate(
+        f'{df["used_milatools_before"].iloc[-1]:.2%}',
+        (df.index[-1], df["used_milatools_before"].iloc[-1]),
+    )
 
-    cluster_suffix = f" on the {cluster} cluster" if cluster else ""
+    ax2.set_title("Number of users using milatools")
 
-    ax2.set_title(f"Cluster users using milatools{cluster_suffix}")
+    df[["using_milatools", "used_milatools_before"]].plot(
+        kind="line", ax=ax1, legend=True, ylabel="Percentage of users using milatools"
+    )
     df[["milatools_users_this_period", "cluster_users_this_period"]].plot(
         kind="area",
         ax=ax2,
         legend=True,
+        # label=["Using milatools", "Not using milatools"],
     )
-    # NOTE: Keys:
-    # "milatools_users_this_period"
-    # "cluster_users_this_period"
-    # "milatools_users_so_far"
-    # "cluster_users_so_far"
-    # "users_this_period_that_used_milatools_before"
-
-    ax1.set_title(f"Percentage of users using milatools{cluster_suffix}")
-    ax1.set_ylim(0, 1)
-
-    df[
-        [
-            "using_milatools",
-            # "percentage_used_before",
-            "used_milatools_before",
-        ]
-    ].plot(
-        kind="line",
-        ax=ax1,
-        legend=True,
-        sharex=True,
-    )
-
-    # df[["percentage_so_far"]].plot(kind="line", ax=ax, legend=True)
-    # daily_counts = df.resample(rule="W-MON")
-
-    # Calculate the percentage of milatools_users relative to cluster_users
-    # df[["milatools_users_so_far", "cluster_users_so_far"]].plot(kind="area", ax=ax2, legend=True)
     # Set x-ticks and labels
-    # ax.set_xticks(range(len(df.index)))  # Set all possible x-tick positions
-    # Apply all labels with rotation
-    ax1.set_xticklabels(df.index.strftime("%Y-%m-%d"), rotation=90)
-    ax2.set_xticklabels(df.index.strftime("%Y-%m-%d"), rotation=90)
 
-    # fig.tight_layout()
+    assert isinstance(df.index, DatetimeIndex)
+    ax1.set_xticks(df.index)  # Set all possible x-tick positions
+    ax2.set_xticks(df.index)  # Set all possible x-tick positions
+    # Apply all labels with rotation
+    label_every_week = df.index.strftime("%Y-%m-%d")
+    label_every_month = [
+        label_every_week[i] if i % 4 == 0 else "" for i in range(len(label_every_week))
+    ]
+    ax1.set_xticklabels(label_every_month, rotation=45)
+    ax2.set_xticklabels(label_every_month, rotation=45)
+
+    fig.tight_layout()
     # fig.layout
     fig_path = Path(
         f"milatools_usage_{period.start_date.date()}_{period.end_date.date()}_{fig_suffix}.png"
     )
-    plt.show()
-    fig.savefig(fig_path)
+    # plt.show()
     fig.set_size_inches(12, 6)
+    fig.savefig(fig_path)
     print(f"Figure saved at {fig_path}")
     return [fig_path]
 
 
-def get_milatools_usage_data(args: Period, cluster: str | list[str] | None):
-    logger.info(
-        f"Getting milatools adoption data from {args.start_date} to {args.end_date}"
-    )
+def _get_milatools_usage_data(args: Period, cluster: str | list[str] | None):
     cluster_suffix = f" on the {cluster} cluster" if cluster else ""
+    logger.info(
+        f"Getting milatools usage data from {args.start_date} to {args.end_date}{cluster_suffix}"
+    )
 
     milatools_users_so_far: set[str] = set()
     cluster_users_so_far: set[str] = set()
@@ -246,7 +243,7 @@ def get_milatools_usage_data(args: Period, cluster: str | list[str] | None):
     for interval_start, interval_end in zip(
         date_range.to_list()[:-1], date_range.to_list()[1:]
     ):
-        milatools_users_that_period, cluster_users_that_period = get_unique_users(
+        milatools_users_that_period, cluster_users_that_period = _get_unique_users(
             interval_start, interval_end, cluster=cluster
         )
         if not cluster_users_that_period:
@@ -311,7 +308,7 @@ def _setup_logging(verbose: int):
             logger.setLevel("DEBUG")
 
 
-def get_unique_users(
+def _get_unique_users(
     start_date: datetime,
     end_date: datetime,
     cluster: str | list[str] | Query | None = None,
@@ -331,8 +328,10 @@ def get_unique_users(
         logger.debug(f"Reading data from {cached_results_path}")
         with cached_results_path.open("rb") as f:
             milatools_users, all_users = pickle.load(f)
-        assert is_iterable_of(milatools_users, str) and isinstance(milatools_users, set)
-        assert is_iterable_of(all_users, str) and isinstance(all_users, set)
+        assert _is_iterable_of(milatools_users, str) and isinstance(
+            milatools_users, set
+        )
+        assert _is_iterable_of(all_users, str) and isinstance(all_users, set)
         return milatools_users, all_users
 
     job_structured_db = jobs_collection()
@@ -366,315 +365,14 @@ def get_unique_users(
     return milatools_users, all_users
 
 
-def make_usage_plots(args: Args, job_name: str = "mila-code") -> list[Path]:
-    date_start = args.start_date  # datetime(year=2024, month=1, day=1, tzinfo=MTL)
-    date_end = args.end_date  # datetime(year=2025, month=1, day=1, tzinfo=MTL)
-
-    unfiltered_jobs = retrieve_data(
-        date_start,
-        date_end,
-        job_name=job_name,
-        cache_dir=Path(os.environ.get("SCRATCH", tempfile.gettempdir())),
-    )
-    filtered_jobs = preprocess_and_filter_jobs(unfiltered_jobs, date_start, date_end)
-    jobs = filtered_jobs
-
-    # Not really necessary.
-    milacode_jobs = [job for job in jobs if job.name == job_name]
-    logger.info(f"We have {len(milacode_jobs)} {job_name} jobs.")
-
-    n_jobs_over_30_minutes = len(
-        list(job for job in milacode_jobs if job.duration.total_seconds() > 30 * 60)
-    )
-    print(n_jobs_over_30_minutes)
-    print(f"We have {n_jobs_over_30_minutes} {job_name} jobs over 30 minutes.")
-
-    milacode_jobs_over_10_minutes = [
-        job for job in jobs if job.duration.total_seconds() >= 10 * 60
-    ]
-    print(
-        f"We have {len(milacode_jobs_over_10_minutes)} {job_name} jobs over 10 minutes."
-    )
-    fig = plot_total_jobs_per_week(milacode_jobs_over_10_minutes, job_name=job_name)
-    name = f"{job_name}-jobs-over-10-minutes.png"
-    fig_path = Path.cwd() / name
-    fig.savefig(fig_path)
-    print(f"Saved figure at {fig_path}.")
-
-    fig = plot_unique_users_each_week(jobs, job_name=job_name)
-    name2 = f"unique-users-{job_name}.png"
-    fig2_path = Path.cwd() / name2
-    fig.savefig(fig2_path)
-    print(f"Saved figure at {fig2_path}.")
-    # print(list(job.duration.total_seconds() for job in L_milacode_jobs))
-    # print(list(job.user for job in L_milacode_jobs))
-    return [fig_path, fig2_path]
-
-
 T = TypeVar("T")
 
 
-def is_iterable_of(v: Any, t: type[T]) -> TypeGuard[Iterable[T]]:
+def _is_iterable_of(v: Any, t: type[T]) -> TypeGuard[Iterable[T]]:
     try:
         return all(isinstance(v_i, t) for v_i in v)
     except TypeError:
         return False
-
-
-def retrieve_data(
-    start: datetime,
-    end: datetime,
-    cache_dir: str | Path,
-    job_name: str = "mila-code",
-    cluster_name: str | None = None,
-) -> list[SlurmJob]:
-    hash = hashlib.md5(f"{start}-{end}-{job_name}-{cluster_name}".encode()).hexdigest()
-    cached_results_path = Path(cache_dir) / f"milatools-jobs-{hash}.pkl"
-    if cached_results_path.exists():
-        print(f"Reading from {cached_results_path}.")
-        with cached_results_path.open("rb") as f_input:
-            unfiltered_jobs = pickle.load(f_input)
-        assert is_iterable_of(unfiltered_jobs, SlurmJob)
-        assert isinstance(unfiltered_jobs, list)
-    else:
-        print(f"Retrieving jobs from {start} to {end} with name {job_name!r}.")
-        unfiltered_jobs = list(
-            get_jobs(start=start, end=end, name=job_name, cluster=cluster_name)
-        )
-        print(f"Writing to {cached_results_path}.")
-        with cached_results_path.open("wb") as f_output:
-            pickle.dump(unfiltered_jobs, f_output, pickle.HIGHEST_PROTOCOL)
-    return unfiltered_jobs
-
-
-def fix_job_start_end_elapsed_time(job: SlurmJob) -> SlurmJob | None:
-    """Fixes the start time, end time, and elapsed time of a job if they are not
-    consistent with each other.
-
-    This is a common issue with jobs coming from SARC.
-    """
-
-    if job.start_time is None:
-        if job.end_time is None:
-            logger.info(f"Job {job.job_id} has no start time or end time.")
-            return None
-        if job.elapsed_time == 0:
-            logger.info(f"Job {job.job_id} has no start time or elapsed time.")
-            return None
-        job = job.replace(start_time=job.end_time - timedelta(seconds=job.elapsed_time))
-        duration_seconds = job_duration_seconds(job)
-        assert job.elapsed_time == duration_seconds
-        return job
-
-    if job.end_time is None:
-        if job.elapsed_time == 0:
-            logger.info(f"Job {job.job_id} has no end time or elapsed time.")
-            return None
-        end_time = job.start_time + timedelta(seconds=job.elapsed_time)
-        job = job.replace(end_time=end_time)
-        duration_seconds = job_duration_seconds(job)
-        assert job.elapsed_time == duration_seconds
-        return job
-
-    duration_seconds = job_duration_seconds(job)
-    if job.elapsed_time == 0:
-        if duration_seconds != 0:
-            logger.info(
-                f"Job {job.job_id} has a no elapsed time but does have a duration? "
-                f"{duration_seconds=} != {job.elapsed_time=}."
-            )
-            return None
-        return job  # job has 0 duration and 0 calc time?
-
-    return job
-
-
-def job_duration_seconds(job: SlurmJob) -> int:
-    assert job.start_time
-    assert job.end_time
-    return int(job.duration.total_seconds())
-
-
-def clip_job_within_bounds(job: SlurmJob, start: datetime, end: datetime) -> SlurmJob:
-    # Clip the job to the time range we are interested in.
-    assert job.start_time and job.end_time
-    if job.start_time < start:
-        logger.info(
-            f"Job {job.job_id}'s Start time is earlier than what we asked for: "
-            f"{job.start_time} < {start}"
-        )
-        job = job.replace(start_time=start)
-        job = job.replace(elapsed_time=job_duration_seconds(job))
-
-    assert job.start_time and job.end_time
-
-    if job.end_time > end:
-        logger.info(
-            f"Job {job.job_id}'s End time is later than what we asked for: "
-            f"{job.end_time} > {end}"
-        )
-        job = job.replace(end_time=end)
-        job = job.replace(elapsed_time=job_duration_seconds(job))
-
-    assert job.start_time and job.end_time
-    return job
-
-
-def preprocess_and_filter_jobs(
-    unfiltered_jobs: Iterable[SlurmJob], start: datetime, end: datetime
-) -> list[SlurmJob]:
-    """Just a simple filter to regulate certain jobs coming from SARC that might not
-    have a proper start time or end time."""
-
-    filtered_jobs: list[SlurmJob] = []
-    job_index = 0
-    for job_index, job in enumerate(unfiltered_jobs):
-        if job.user == "normandf":
-            # don't count jobs run for unit tests
-            continue
-
-        job = fix_job_start_end_elapsed_time(job)
-        if not job:
-            continue
-
-        job = clip_job_within_bounds(job, start, end)
-
-        # We only care about jobs that actually ran.
-        if job.elapsed_time <= 0:
-            continue
-
-        # LD_jobs_output.append(job.json())
-        filtered_jobs.append(job)
-
-    print(f"Filtered {len(filtered_jobs)} out of a total of {job_index+1} jobs.")
-    return filtered_jobs
-
-
-def plot_total_jobs_per_week(
-    jobs: list[SlurmJob], job_name: str
-) -> matplotlib.figure.Figure:
-    df = pd.DataFrame(
-        [job.start_time for job in jobs],
-        columns=["Timestamp"],
-    )
-
-    df.set_index("Timestamp", inplace=True)
-    # daily_counts = df.resample(rule="D").size()
-    daily_counts = df.resample(rule="W-MON").size()
-    daily_counts.index = daily_counts.index.strftime("%Y-%m-%d")
-
-    fig, ax = plt.subplots()
-    daily_counts.plot(kind="bar", ax=ax)
-    # ax.set_xlabel('Date')
-    ax.set_ylabel("jobs")
-    ax.set_title(f'"{job_name}" jobs over 10 minutes in duration each week')
-
-    # ticks_to_show = daily_counts.index[::5]  # Show every 5th label
-    ticks_to_show = daily_counts.index  # Show every 5th label
-    ax.set_xticks(range(len(daily_counts.index)))  # Set all possible x-tick positions
-    ax.set_xticklabels(
-        daily_counts.index, rotation=90
-    )  # Apply all labels with rotation
-    ax.set_xticklabels(
-        [label if label in ticks_to_show else "" for label in daily_counts.index]
-    )  # Hide non-selected labels
-    # plt.tight_layout()
-    fig.tight_layout()
-    return fig
-
-    # Plot the histogram
-    # daily_counts.plot(kind='bar')
-    # plt.xlabel('Day')
-    # plt.ylabel('Number of Items')
-    # plt.title('Items by Day')
-    # plt.xticks(rotation=45)  # Rotate labels to improve readability
-    # plt.show()
-
-    # Let's not care about the fact that usernames might differ between clusters.
-    # That would be perfectionism at this early exploratory moment.
-
-
-def plot_unique_users_each_week(
-    jobs: list[SlurmJob], job_name: str
-) -> matplotlib.figure.Figure:
-    # unique_users_each_day: dict[str, set[str]] = defaultdict(set)
-    # for job in jobs:
-    #     assert job.start_time
-    #     day = job.start_time.strftime("%Y-%m-%d")
-    #     unique_users_each_day[day].add(job.user)
-
-    df = pd.DataFrame(
-        {
-            "job_id": [job.job_id for job in jobs],
-            "user": [job.user for job in jobs],
-            "start_date": [job.start_time.strftime("%Y-%m-%d") for job in jobs],
-        }
-    )
-    df.set_index("job_id", inplace=True)
-    df["start_date"] = pd.to_datetime(df["start_date"])
-
-    # Unique users each week:
-    df = df.groupby(pd.Grouper(key="start_date", freq="W-MON"))["user"].nunique()
-
-    daily_counts = df.sort_index()
-    ax: matplotlib.axes._axes.Axes
-    fig, ax = plt.subplots()
-    daily_counts.plot(kind="bar", ax=ax)
-    # ax.set_xlabel('Date')
-    ax.set_ylabel("Users")
-    ax.set_title(f'Unique users of "{job_name}" each week')
-
-    ax.set_xticks(range(len(daily_counts.index)))  # Set all possible x-tick positions
-    ax.set_xticklabels(
-        daily_counts.index.strftime("%Y-%m-%d"), rotation=90
-    )  # Apply all labels with rotation
-
-    fig.tight_layout()
-    return fig
-
-
-def upload_figures_to_google_drive(figures: list[Path]):
-    # gauth = GoogleAuth()
-    # drive = GoogleDrive(gauth)
-
-    from google.oauth2.credentials import Credentials
-    from googleapiclient.discovery import build
-    from googleapiclient.errors import HttpError
-    from googleapiclient.http import MediaFileUpload
-
-    # SCOPES = ["https://www.googleapis.com/auth/drive.metadata.write"]
-
-    # creds = os.environ.get("GOOGLE_DRIVE_API_KEY")
-    try:
-        creds = Credentials.from_authorized_user_file("token.json")
-    except Exception as exception:
-        print(
-            "Unable to authenticate with google drive API, skipping uploading figures."
-        )
-        print(f"exc: {exception}")
-        return
-
-    try:
-        # create drive api client
-        service = build("drive", "v3", credentials=creds)
-
-        for figure in figures:
-            file_metadata = {"name": figure.name}
-            media = MediaFileUpload(
-                filename=figure.name,
-                mimetype=f"image/{figure.suffix.removeprefix('.')}",
-            )
-            # pylint: disable=maybe-no-member
-            file = (
-                service.files()
-                .create(body=file_metadata, media_body=media, fields="id")
-                .execute()
-            )
-            print(f'File ID: {file.get("id")}')
-
-    except HttpError as error:
-        print(f"An error occurred: {error}")
-        file = None
 
 
 if __name__ == "__main__":
