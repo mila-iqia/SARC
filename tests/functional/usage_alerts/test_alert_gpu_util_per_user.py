0a2326e40565421eb768d0bea33f5fc381b2cc56
notoraptor <stevenbocco@gmail.com>
2024-09-19 16:04:02 -0400

    Use file_regression for tests.

--- tests/functional/usage_alerts/test_alert_gpu_util_per_user.py
+++ tests/functional/usage_alerts/test_alert_gpu_util_per_user.py
@@ -1,4 +1,5 @@
 import functools
+import re
 from datetime import timedelta
 
 import pytest
@@ -8,67 +9,31 @@ from sarc.client import get_jobs
 from tests.functional.jobs.test_func_load_job_series import MOCK_TIME
 
 from ..jobs.test_func_job_statistics import generate_fake_timeseries
-from .common import _get_warnings
-
-get_warnings = functools.partial(
-    _get_warnings,
-    module="sarc.alerts.usage_alerts.gpu_util_per_user:gpu_util_per_user.py",
-)
 
 
 @pytest.mark.freeze_time(MOCK_TIME)
 @pytest.mark.usefixtures("read_only_db", "tzlocal_is_mtl")
 @pytest.mark.parametrize(
-    "params,expected",
+    "params",
     [
         # Check with default params. In last 7 days from now (mock time: 2023-11-22),
         # there is only 2 jobs, both with no gpu_utilization, so, no warnings.
-        (dict(threshold=timedelta()), []),
+        dict(threshold=timedelta()),
         # Check with no time_interval and a threshold to 7 days
-        (
-            dict(threshold=timedelta(hours=7), time_interval=None),
-            [
-                "[beaubonhomme] insufficient average gpu_util: 21585.0 GPU-seconds; minimum required: 7:00:00 (25200.0 GPU-seconds)",
-                "[bonhomme] insufficient average gpu_util: 21585.0 GPU-seconds; minimum required: 7:00:00 (25200.0 GPU-seconds)",
-                "[grosbonhomme] insufficient average gpu_util: 21585.0 GPU-seconds; minimum required: 7:00:00 (25200.0 GPU-seconds)",
-                "[petitbonhomme] insufficient average gpu_util: 22784.166666666668 GPU-seconds; minimum required: 7:00:00 (25200.0 GPU-seconds)",
-            ],
-        ),
+        dict(threshold=timedelta(hours=7), time_interval=None),
         # Check with no time_interval and threshold to 6 days
-        (
-            dict(threshold=timedelta(hours=6), time_interval=None),
-            [
-                "[beaubonhomme] insufficient average gpu_util: 21585.0 GPU-seconds; minimum required: 6:00:00 (21600.0 GPU-seconds)",
-                "[bonhomme] insufficient average gpu_util: 21585.0 GPU-seconds; minimum required: 6:00:00 (21600.0 GPU-seconds)",
-                "[grosbonhomme] insufficient average gpu_util: 21585.0 GPU-seconds; minimum required: 6:00:00 (21600.0 GPU-seconds)",
-                # "[petitbonhomme]
-            ],
-        ),
+        dict(threshold=timedelta(hours=6), time_interval=None),
         # Check with a valid time_interval
-        (
-            dict(threshold=timedelta(hours=8), time_interval=timedelta(days=276)),
-            [
-                "[beaubonhomme] insufficient average gpu_util: 19816.229166666668 GPU-seconds; minimum required: 8:00:00 (28800.0 GPU-seconds)",
-                "[grosbonhomme] insufficient average gpu_util: 9023.729166666666 GPU-seconds; minimum required: 8:00:00 (28800.0 GPU-seconds)",
-                "[petitbonhomme] insufficient average gpu_util: 28780.0 GPU-seconds; minimum required: 8:00:00 (28800.0 GPU-seconds)",
-            ],
-        ),
+        dict(threshold=timedelta(hours=8), time_interval=timedelta(days=276)),
         # Check will all params, including minimum_runtime
-        (
-            dict(
-                threshold=timedelta(hours=8),
-                time_interval=timedelta(days=276),
-                minimum_runtime=timedelta(seconds=39000),
-            ),
-            [
-                "[beaubonhomme] insufficient average gpu_util: 19816.229166666668 GPU-seconds; minimum required: 8:00:00 (28800.0 GPU-seconds)",
-                # "[grosbonhomme] insufficient average gpu_util: 9023.729166666666 GPU-seconds; minimum required: 8:00:00 (28800.0 GPU-seconds)",
-                "[petitbonhomme] insufficient average gpu_util: 28780.0 GPU-seconds; minimum required: 8:00:00 (28800.0 GPU-seconds)",
-            ],
+        dict(
+            threshold=timedelta(hours=8),
+            time_interval=timedelta(days=276),
+            minimum_runtime=timedelta(seconds=39000),
         ),
     ],
 )
-def test_alert_gpu_util_per_user(params, expected, caplog, monkeypatch):
+def test_alert_gpu_util_per_user(params, caplog, monkeypatch, file_regression):
     monkeypatch.setattr(
         "sarc.jobs.series.get_job_time_series", generate_fake_timeseries
     )
@@ -77,4 +42,10 @@ def test_alert_gpu_util_per_user(params, expected, caplog, monkeypatch):
         job.statistics(save=True)
 
     check_gpu_util_per_user(**params)
-    assert get_warnings(caplog.text) == expected
+    file_regression.check(
+        re.sub(
+            r"WARNING +sarc\.alerts\.usage_alerts\.gpu_util_per_user:gpu_util_per_user.py:[0-9]+ +",
+            "",
+            caplog.text,
+        )
+    )
