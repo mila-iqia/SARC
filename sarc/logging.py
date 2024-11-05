1a0a2801cc4af1a1b926f8d6b5e8eb96997177f5
Bruno Carrez <bruno.carrez@mila.quebec>
2024-11-05 09:20:47 -0500

    wip

--- sarc/logging.py
+++ sarc/logging.py
@@ -79,22 +79,6 @@ def setupLogging():
     from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
     from opentelemetry.sdk.resources import Resource
 
-    logger_provider = LoggerProvider(
-        resource=Resource.create(
-            {
-                "service.name": config().loki.service_name,
-                "service.instance.id": os.uname().nodename,
-            }
-        ),
-    )
-    set_logger_provider(logger_provider)
-
-    endpoint = config().loki.endpoint
-
-    otlp_exporter = OTLPLogExporter(endpoint)
-    logger_provider.add_log_record_processor(BatchLogRecordProcessor(otlp_exporter))
-    handler = LoggingHandler(level=logging.NOTSET, logger_provider=logger_provider)
-
     logging_levels = {
         "DEBUG": logging.DEBUG,
         "INFO": logging.INFO,
@@ -102,12 +86,38 @@ def setupLogging():
         "ERROR": logging.ERROR,
         "CRITICAL": logging.CRITICAL,
     }
-    log_level = logging_levels.get(config().logging.log_level, logging.INFO)
-    logging.basicConfig(
-        handlers=[handler, logging.StreamHandler()],
-        format="%(asctime)-15s::%(levelname)s::%(name)s::%(message)s",
-        level=log_level,
-    )
+    log_level = logging.WARNING # default log level
+    if config().logging:
+        log_level = logging_levels.get(config().logging.log_level, logging.WARNING)
+
+    if (config().loki):
+        logger_provider = LoggerProvider(
+            resource=Resource.create(
+                {
+                    "service.name": config().loki.service_name,
+                    "service.instance.id": os.uname().nodename,
+                }
+            ),
+        )
+        set_logger_provider(logger_provider)
+
+        endpoint = config().loki.endpoint
+
+        otlp_exporter = OTLPLogExporter(endpoint)
+        logger_provider.add_log_record_processor(BatchLogRecordProcessor(otlp_exporter))
+        handler = LoggingHandler(level=logging.NOTSET, logger_provider=logger_provider)
+
+        logging.basicConfig(
+            handlers=[handler, logging.StreamHandler()],
+            format="%(asctime)-15s::%(levelname)s::%(name)s::%(message)s",
+            level=log_level,
+        )
+    else:
+        logging.basicConfig(
+            handlers=[logging.StreamHandler()],
+            format="%(asctime)-15s::%(levelname)s::%(name)s::%(message)s",
+            level=logging.INFO,
+        )
 
     # logging.info("SARC Test info log")
     # logging.debug("SARC Test debug log")
