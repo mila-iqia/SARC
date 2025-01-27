3ab2c68ceb8f50f42e57e08c4f578c9e2e71dda5
Bruno Carrez <bruno.carrez@mila.quebec>
2025-01-26 19:12:25 -0500

    Add test_loki_logging_handler

--- sarc/logging.py
+++ sarc/logging.py
@@ -4,12 +4,30 @@ import os
 from sarc.config import config
 
 
-def setupLogging():
+def getOpenTelemetryLoggingHandler(log_level=logging.WARNING):
+
     from opentelemetry._logs import set_logger_provider
     from opentelemetry.exporter.otlp.proto.http._log_exporter import OTLPLogExporter
     from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
     from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
     from opentelemetry.sdk.resources import Resource
+
+    logger_provider = LoggerProvider(
+        resource=Resource.create(
+            {
+                "service.name": config().logging.service_name,
+                "service.instance.id": os.uname().nodename,
+            }
+        ),
+    )
+    set_logger_provider(logger_provider)
+
+    otlp_exporter = OTLPLogExporter(config().logging.OTLP_endpoint)
+    logger_provider.add_log_record_processor(BatchLogRecordProcessor(otlp_exporter))
+    return LoggingHandler(level=log_level, logger_provider=logger_provider)
+
+
+def setupLogging():
 
     logging_levels = {
         "DEBUG": logging.DEBUG,
@@ -21,25 +39,23 @@ def setupLogging():
 
     if config().logging:
         log_level = logging_levels.get(config().logging.log_level, logging.WARNING)
-        logger_provider = LoggerProvider(
-            resource=Resource.create(
-                {
-                    "service.name": config().logging.service_name,
-                    "service.instance.id": os.uname().nodename,
-                }
-            ),
-        )
-        set_logger_provider(logger_provider)
 
-        otlp_exporter = OTLPLogExporter(config().logging.OTLP_endpoint)
-        logger_provider.add_log_record_processor(BatchLogRecordProcessor(otlp_exporter))
-        handler = LoggingHandler(level=log_level, logger_provider=logger_provider)
+        handler = getOpenTelemetryLoggingHandler(log_level)
+
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
 
         logging.basicConfig(
-            handlers=[handler, logging.StreamHandler()],
+            handlers=[handler, console_handler],
             format="%(asctime)-15s::%(levelname)s::%(name)s::%(message)s",
             level=log_level,
         )
+
+        logging.error("ERROR test log")
+
     else:
         logging.basicConfig(
             handlers=[logging.StreamHandler()],
--- tests/functional/test_loki_logging.py
+++ tests/functional/test_loki_logging.py
@@ -1,44 +1,60 @@
 # test logging to loki service
 
 import logging
+import time
 
 import pytest
 import requests_mock
 from pytest_httpserver import HTTPServer
 
 from sarc.config import config
-from sarc.logging import setupLogging
+from sarc.logging import getOpenTelemetryLoggingHandler, setupLogging
 
 
 @pytest.fixture
 def httpserver(httpserver: HTTPServer):
 
     # Configurer le mock pour l'endpoint Loki
-    httpserver.expect_request("/otlp/v1/logs").respond_with_json(
+    httpserver.expect_request("/otlp/v1/logs", method="POST").respond_with_json(
         {"status": "success", "data": {"result": []}}
     )
     return httpserver
 
 
+import requests
+
 from sarc.logging import setupLogging
 
 
-# @pytest.mark.no_capture
-# @pytest.mark.skip(reason="cannot make HTTPServer running without error in this situation")
-def test_loki_logging(
-    standard_config,
-):
+def test_loki_logging_handler(standard_config, httpserver):
     # Configurer l'URL de l'endpoint Loki
     # print(f"http://{httpserver.host}:{httpserver.port}/otlp/v1/logs")
-    # print(httpserver.url_for("/otlp/v1/logs"))
     loki_url = httpserver.url_for("/otlp/v1/logs")
     config().logging.OTLP_endpoint = loki_url
 
-    setupLogging()
+    ot_handler = getOpenTelemetryLoggingHandler()
 
-    logging.warning("test Warning message")
+    ot_handler.flush()
+    assert len(httpserver.log) == 0
 
-    # assert loki_mock.called
-    # assert loki_mock.call_count == 1
-    # assert loki_mock.request_history[0].url == 'http://loki01.server.raisin.quebec:3100/otlp/v1/logs'
-    assert True
+    ot_handler.emit(
+        logging.LogRecord(
+            "test_ERROR", logging.ERROR, "test_ERROR", 0, "test ERROR log", [], None
+        )
+    )
+    ot_handler.flush()
+    assert len(httpserver.log) == 1
+
+    ot_handler.emit(
+        logging.LogRecord(
+            "test_WARNING",
+            logging.WARNING,
+            "test_WARNING",
+            0,
+            "test WARNING log",
+            [],
+            None,
+        )
+    )
+    ot_handler.flush()
+    assert len(httpserver.log) == 2
