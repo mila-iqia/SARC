bafb20e11926572c702343b5f005df16d4d3df9f
notoraptor <stevenbocco@gmail.com>
2025-03-16 06:48:41 -0400

    [SARC-397] Modifier la commande `acquire jobs` pour supporter des périodes de temps inférieures à la journée (à la minute près)

--- sarc/cli/acquire/jobs.py
+++ sarc/cli/acquire/jobs.py
@@ -17,6 +17,10 @@ def _str_to_dt(dt_str: str) -> datetime:
     return datetime.strptime(dt_str, "%Y-%m-%d")
 
 
+def _str_to_extended_dt(dt_str: str) -> datetime:
+    return datetime.strptime(dt_str, "%Y-%m-%dT%H:%M")
+
+
 def parse_dates(dates: list[str], cluster_name: str) -> list[(datetime, bool)]:
     parsed_dates = []  # return values are tuples (date, is_auto)
     for date in dates:
@@ -84,6 +88,10 @@ class AcquireJobs:
 
     dates: list[str] = field(alias=["-d"], default_factory=list)
 
+    time_from: str = field(alias=["-a"], default=None)
+    time_to: str = field(alias=["-b"], default=None)
+    user: str = field(alias=["-u"], default=None)
+
     no_prometheus: bool = field(
         alias=["-s"],
         action="store_true",
@@ -91,34 +99,90 @@ class AcquireJobs:
     )
 
     def execute(self) -> int:
+        time_from = None
+        time_to = None
+        if self.dates:
+            if self.time_from or self.time_to:
+                logging.error(
+                    "Parameters mutually exclusive: --date | (--time_from, --time_to)"
+                )
+                return -1
+        elif self.time_from or self.time_to:
+            if not self.time_from or not self.time_to:
+                logging.error("Both parameters needed: --time_from, --time_to")
+                return -1
+            time_from = _str_to_extended_dt(self.time_from)
+            time_to = _str_to_extended_dt(self.time_to)
+            if time_from >= time_to:
+                logging.error(
+                    f"Expected time_from < time_to, instead got time_from: {time_from}, time_to: {time_to}"
+                )
+                return -1
+            interval = time_to - time_from
+            if interval > timedelta(hours=1):
+                logging.error(
+                    f"You try to scrap from more than 1 hour ({interval}). Instead consider scraping entire days using argument --date."
+                )
+                return -1
+
         cfg = config()
         clusters_configs = cfg.clusters
 
         for cluster_name in self.cluster_names:
             try:
-                for date, is_auto in parse_dates(self.dates, cluster_name):
+                if time_from is not None:
                     with using_trace(
-                        "AcquireJobs", "acquire_cluster_data", exception_types=()
+                        "AcquireJobs",
+                        "acquire_cluster_data_from_interval",
+                        exception_types=(),
                     ) as span:
                         span.set_attribute("cluster_name", cluster_name)
-                        span.set_attribute("date", str(date))
-                        span.set_attribute("is_auto", is_auto)
+                        span.set_attribute("time_from", str(time_from))
+                        span.set_attribute("time_to", str(time_to))
                         try:
                             logging.info(
-                                f"Acquire data on {cluster_name} for date: {date} (is_auto={is_auto})"
+                                f"Acquire data on {cluster_name} for interval: {time_from} to {time_to}"
                             )
 
                             sacct_mongodb_import(
-                                clusters_configs[cluster_name], date, self.no_prometheus
+                                clusters_configs[cluster_name],
+                                time_from,
+                                self.no_prometheus,
+                                time_to,
+                                self.user,
                             )
-                            if is_auto:
-                                _dates_set_last_date(cluster_name, date)
                         # pylint: disable=broad-exception-caught
                         except Exception as e:
                             logging.error(
-                                f"Failed to acquire data for {cluster_name} on {date}: {e}"
+                                f"Failed to acquire data for {cluster_name} in interval {time_from} to {time_to}: {e}"
                             )
                             raise e
+                else:
+                    for date, is_auto in parse_dates(self.dates, cluster_name):
+                        with using_trace(
+                            "AcquireJobs", "acquire_cluster_data", exception_types=()
+                        ) as span:
+                            span.set_attribute("cluster_name", cluster_name)
+                            span.set_attribute("date", str(date))
+                            span.set_attribute("is_auto", is_auto)
+                            try:
+                                logging.info(
+                                    f"Acquire data on {cluster_name} for date: {date} (is_auto={is_auto})"
+                                )
+
+                                sacct_mongodb_import(
+                                    clusters_configs[cluster_name],
+                                    date,
+                                    self.no_prometheus,
+                                )
+                                if is_auto:
+                                    _dates_set_last_date(cluster_name, date)
+                            # pylint: disable=broad-exception-caught
+                            except Exception as e:
+                                logging.error(
+                                    f"Failed to acquire data for {cluster_name} on {date}: {e}"
+                                )
+                                raise e
             # pylint: disable=broad-exception-caught
             except Exception:
                 # Error while acquiring data on a cluster from given dates.
--- sarc/jobs/sacct.py
+++ sarc/jobs/sacct.py
@@ -31,7 +31,13 @@ class SAcctScraper:
     The scraper is currently hard-coded to fetch data for a day.
     """
 
-    def __init__(self, cluster: ClusterConfig, day: datetime):
+    def __init__(
+        self,
+        cluster: ClusterConfig,
+        start: datetime,
+        end: datetime = None,
+        user: str = None,
+    ):
         """Initialize a SAcctScraper.
 
         Arguments:
@@ -41,9 +47,23 @@ class SAcctScraper:
                 00:00 on the next day.
         """
         self.cluster = cluster
-        self.day = day
-        self.start = datetime.combine(day, time.min)
-        self.end = self.start + timedelta(days=1)
+        if end is None:
+            day = start
+            self.day = day
+            self.start = datetime.combine(day, time.min)
+            self.end = self.start + timedelta(days=1)
+        else:
+            self.day = None
+            self.start = start
+            self.end = end
+        self.user = user
+        self.get_raw = with_cache(
+            self.fetch_raw,
+            subdirectory="sacct",
+            key=self._cache_key,
+            live=True,
+            on_disk=(self.user is None),
+        )
 
     @trace_decorator()
     def fetch_raw(self) -> dict:
@@ -53,7 +73,8 @@ class SAcctScraper:
         end = self.end.strftime(fmt)
         accounts = self.cluster.accounts and ",".join(self.cluster.accounts)
         accounts_option = f"-A {accounts}" if accounts else ""
-        cmd = f"{self.cluster.sacct_bin} {accounts_option} -X -S {start} -E {end} --allusers --json"
+        user_option = "--allusers" if self.user is None else f"--user {self.user}"
+        cmd = f"{self.cluster.sacct_bin} {accounts_option} -X -S {start} -E {end} {user_option} --json"
         logger.info(f"{self.cluster.name} $ {cmd}")
         if self.cluster.host == "localhost":
             results = subprocess.run(
@@ -65,16 +86,17 @@ class SAcctScraper:
 
     def _cache_key(self):
         today = datetime.combine(date.today(), datetime.min.time())
-        if self.day < today:
+        if self.day is None:
+            fmt = "%Y-%m-%dT%H:%M"
+            startstr = self.start.strftime(fmt)
+            endstr = self.end.strftime(fmt)
+            return f"{self.cluster.name}.{startstr}.{endstr}.user_{self.user}.json"
+        elif self.day < today:
             daystr = self.day.strftime("%Y-%m-%d")
             return f"{self.cluster.name}.{daystr}.json"
         else:
             # Not cachable
             return None
-
-    @with_cache(subdirectory="sacct", key=_cache_key, live=True)
-    def get_raw(self) -> dict:
-        return self.fetch_raw()
 
     def __len__(self) -> int:
         return len(self.get_raw()["jobs"])
@@ -247,7 +269,11 @@ class SAcctScraper:
 
 @trace_decorator()
 def sacct_mongodb_import(
-    cluster: ClusterConfig, day: datetime, no_prometheus: bool
+    cluster: ClusterConfig,
+    start: datetime,
+    no_prometheus: bool,
+    end: datetime = None,
+    user: str = None,
 ) -> None:
     """Fetch sacct data and store it in MongoDB.
 
@@ -262,7 +288,7 @@ def sacct_mongodb_import(
         If True, avoid any scraping requiring prometheus connection.
     """
     collection = _jobs_collection()
-    scraper = SAcctScraper(cluster, day)
+    scraper = SAcctScraper(cluster, start, end, user)
     logger.info("Getting the sacct data...")
     scraper.get_raw()
     logger.info(
