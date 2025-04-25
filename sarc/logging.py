6ccef54b57b4af385779c0f1d255024baf03c3c1
Bruno Carrez <bruno.carrez@mila.quebec>
2025-04-25 18:26:24 -0400

    lint

--- sarc/logging.py
+++ sarc/logging.py
@@ -11,7 +11,6 @@ from sarc.config import config
 
 
 def getOpenTelemetryLoggingHandler(log_level=logging.WARNING):
-
     logger_provider = LoggerProvider(
         resource=Resource.create(
             {
@@ -39,7 +38,6 @@ def setupLogging(verbose_level: int = 0):
     }
 
     if config().logging:
-
         config_log_level = logging_levels.get(
             config().logging.log_level, logging.WARNING
         )
