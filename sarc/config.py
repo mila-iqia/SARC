18d379a1a299f43b81fb3975438af627f2e20324
Arnaud Bergeron <abergeron@gmail.com>
2025-07-28 14:35:22 -0400

    WIP mila ldap plugin

--- sarc/config.py
+++ sarc/config.py
@@ -196,6 +196,11 @@ class LoggingConfig:
 
 
 @dataclass
+class UserScrapingConfig:
+    scrapers: dict[str, Any]
+
+
+@dataclass
 class ClientConfig:
     mongo: MongoConfig
     cache: Path | None = None
