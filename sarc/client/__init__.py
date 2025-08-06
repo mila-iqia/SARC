b469dca150ad7e364d66aadd22e1efa03cc41401
Arnaud Bergeron <abergeron@gmail.com>
2025-08-06 11:41:53 -0400

    Fix dependecies from the user change

--- sarc/client/__init__.py
+++ sarc/client/__init__.py
@@ -6,15 +6,12 @@ from .series import (
     load_job_series,
     update_job_series_rgu,
 )
-from .users.api import get_user, get_users
 
 __all__ = [
     "count_jobs",
     "get_available_clusters",
     "get_job",
     "get_jobs",
-    "get_user",
-    "get_users",
     "get_rgus",
     "get_cluster_gpu_billings",
     "load_job_series",
