d941918a51ee6db8b61788cab8cc7555f0039f5f
notoraptor <stevenbocco@gmail.com>
2025-10-01 11:53:51 -0400

    Set gpu-year as integers Remove unused data_regression in test

--- tests/functional/allocations/test_update_allocations.py
+++ tests/functional/allocations/test_update_allocations.py
@@ -23,7 +23,7 @@ def test_update_allocations(data_regression):
 
 @pytest.mark.freeze_time("2023-02-15")
 @pytest.mark.usefixtures("empty_read_write_db")
-def test_update_allocations_dry(data_regression, caplog):
+def test_update_allocations_dry(caplog):
     """Test that --dry deactivates data update."""
     assert get_allocations(cluster_name=["fromage", "patate"]) == []
     with caplog.at_level("INFO"):
