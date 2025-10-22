fb177d5d15f42cfd8ec83dcc45e538e0c278c7fc
Arnaud Bergeron <abergeron@gmail.com>
2025-10-22 17:10:33 -0400

    Update the cli tests

--- tests/functional/diskusage/test_update_diskusage.py
+++ tests/functional/diskusage/test_update_diskusage.py
@@ -26,14 +26,8 @@ def test_update_drac_diskusage_one(file_regression, cli_main, remote, enabled_ca
         out=str.encode(raw_report),
     )
 
-    cli_main(
-        [
-            "acquire",
-            "storages",
-            "-c",
-            "gerudo",
-        ]
-    )
+    cli_main(["fetch", "diskusage", "-c", "gerudo"])
+    cli_main(["parse", "diskusage", "--from", "2023-05-11"])
 
     data = get_diskusages(cluster_name=["gerudo", "hyrule"])
     assert len(data) == 1
@@ -41,7 +35,7 @@ def test_update_drac_diskusage_one(file_regression, cli_main, remote, enabled_ca
 
 
 @pytest.mark.usefixtures("empty_read_write_db")
-@pytest.mark.freeze_time("2023-05-12")
+@pytest.mark.freeze_time("2023-05-12", auto_tick_seconds=1)
 def test_update_drac_diskusage_two(file_regression, cli_main, remote, enabled_cache):
     assert get_diskusages(cluster_name=["gerudo", "hyrule"]) == []
 
@@ -68,22 +62,9 @@ def test_update_drac_diskusage_two(file_regression, cli_main, remote, enabled_ca
         ),
     )
 
-    cli_main(
-        [
-            "acquire",
-            "storages",
-            "-c",
-            "gerudo",
-        ]
-    )
-    cli_main(
-        [
-            "acquire",
-            "storages",
-            "-c",
-            "hyrule",
-        ]
-    )
+    cli_main(["fetch", "diskusage", "-c", "gerudo"])
+    cli_main(["fetch", "diskusage", "-c", "hyrule"])
+    cli_main(["parse", "diskusage", "--from", "2023-05-11"])
     data = get_diskusages(cluster_name=["gerudo", "hyrule"])
     assert len(data) == 2
     data_json = "\n".join(
@@ -96,8 +77,10 @@ def test_update_drac_diskusage_two(file_regression, cli_main, remote, enabled_ca
 
 
 @pytest.mark.usefixtures("empty_read_write_db")
-@pytest.mark.freeze_time("2023-05-12")
-def test_update_drac_diskusage_no_duplicate(file_regression, cli_main, remote):
+@pytest.mark.freeze_time("2023-05-12", auto_tick_seconds=1)
+def test_update_drac_diskusage_no_duplicate(
+    file_regression, cli_main, remote, enabled_cache
+):
     assert get_diskusages(cluster_name=["gerudo", "hyrule"]) == []
 
     # Load the expected report content
@@ -118,22 +101,9 @@ def test_update_drac_diskusage_no_duplicate(file_regression, cli_main, remote):
         ],
     )
 
-    cli_main(
-        [
-            "acquire",
-            "storages",
-            "-c",
-            "gerudo",
-        ]
-    )
-    cli_main(
-        [
-            "acquire",
-            "storages",
-            "-c",
-            "gerudo",
-        ]
-    )
+    cli_main(["fetch", "diskusage", "-c", "gerudo"])
+    cli_main(["fetch", "diskusage", "-c", "gerudo"])
+    cli_main(["parse", "diskusage", "--from", "2023-05-11"])
 
     data = get_diskusages(cluster_name=["gerudo", "hyrule"])
     assert len(data) == 1
