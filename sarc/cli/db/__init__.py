fdbde836af28478cffd06b045b19177deaed2ddc
notoraptor <stevenbocco@gmail.com>
2025-10-24 10:03:21 -0400

    Move prometheus backup/restore scripts into `scripts` folder. Remove unit tests.

--- sarc/cli/db/__init__.py
+++ sarc/cli/db/__init__.py
@@ -5,19 +5,17 @@ from simple_parsing import subparsers
 from sarc.cli.db.backup import DbBackup
 from sarc.cli.db.init import DbInit
 from sarc.cli.db.restore import DbRestore
-from sarc.cli.db.prometheus import DbPrometheus
 
 
 @dataclass
 class Db:
     """this is help"""
 
-    command: DbInit | DbBackup | DbRestore | DbPrometheus = subparsers(
+    command: DbInit | DbBackup | DbRestore = subparsers(
         {
             "init": DbInit,
             "backup": DbBackup,
             "restore": DbRestore,
-            "prometheus": DbPrometheus,
         }
     )
 
