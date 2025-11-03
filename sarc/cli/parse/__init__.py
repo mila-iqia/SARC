6c8b7aaa89bf70522c32c592b86b766b74fac22c
Soline <soline_blanc@outlook.fr>
2025-11-03 16:07:31 -0500

    Add ParseJobs calls to the parsing processus

--- sarc/cli/parse/__init__.py
+++ sarc/cli/parse/__init__.py
@@ -3,16 +3,14 @@ from dataclasses import dataclass
 from simple_parsing import subparsers
 
 from .slurmconfig import ParseSlurmConfig
+from .jobs import ParseJobs
 from .users import ParseUsers
 
 
 @dataclass
 class Parse:
-    command: ParseUsers | ParseSlurmConfig = subparsers(
-        {
-            "users": ParseUsers,
-            "slurmconfig": ParseSlurmConfig,
-        }
+    command: ParseUsers | ParseSlurmConfig | ParseJobs = subparsers(
+        {"users": ParseUsers, "slurmconfig": ParseSlurmConfig, "jobs": ParseJobs}
     )
 
     def execute(self) -> int:
