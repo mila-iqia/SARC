62f2a122f75579bda843b88b5a901d906f5666a3
Bruno Carrez <bruno.carrez@mila.quebec>
2025-12-01 23:46:51 -0500

    update systemd script

--- scripts/systemd/scrapers.sh
+++ scripts/systemd/scrapers.sh
@@ -2,7 +2,11 @@
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd $SCRIPTPATH/../../
-# scraping jobs
-sudo -u sarc SARC_MODE=scraping SARC_CONFIG=$SCRIPTPATH/../../config/sarc-prod.yaml ../.local/bin/uv run sarc acquire jobs -c narval mila -d auto
-# scraping users
-sudo -u sarc SARC_MODE=scraping SARC_CONFIG=$SCRIPTPATH/../../config/sarc-prod.yaml ../.local/bin/uv run sarc acquire users
+# fetch jobs
+sudo -u sarc SARC_MODE=scraping SARC_CONFIG=$SCRIPTPATH/../../config/sarc-prod.yaml ../.local/bin/uv run sarc acquire jobs -c narval mila -a 1440
+# fetch prometheus
+sudo -u sarc SARC_MODE=scraping SARC_CONFIG=$SCRIPTPATH/../../config/sarc-prod.yaml ../.local/bin/uv run sarc acquire prometheus -c narval mila -a 1440
+# fetch users
+sudo -u sarc SARC_MODE=scraping SARC_CONFIG=$SCRIPTPATH/../../config/sarc-prod.yaml ../.local/bin/uv run sarc fetch users
+# parse users
+sudo -u sarc SARC_MODE=scraping SARC_CONFIG=$SCRIPTPATH/../../config/sarc-prod.yaml ../.local/bin/uv run sarc parse users --since 2025-11-28
