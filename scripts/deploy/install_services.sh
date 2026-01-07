3a36776ac9c0e1f3e8e20dd1471b6bf9093a484a
Bruno Carrez <bruno.carrez@mila.quebec>
2026-01-07 13:51:04 -0500

    rename systemd scripts "daily/hourly" to "lowfreq/hifreq"

--- scripts/deploy/install_services.sh
+++ scripts/deploy/install_services.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
-ln -s -f $SCRIPTPATH/../systemd/sarc_scrapers_daily.service /etc/systemd/system/sarc_scrapers_daily.service
-ln -s -f $SCRIPTPATH/../systemd/sarc_scrapers_daily.timer /etc/systemd/system/sarc_scrapers_daily.timer
-ln -s -f $SCRIPTPATH/../systemd/sarc_scrapers_hourly.service /etc/systemd/system/sarc_scrapers_hourly.service
-ln -s -f $SCRIPTPATH/../systemd/sarc_scrapers_hourly.timer /etc/systemd/system/sarc_scrapers_hourly.timer
+ln -s -f $SCRIPTPATH/../systemd/sarc_scrapers_hifreq.service /etc/systemd/system/sarc_scrapers_hifreq.service
+ln -s -f $SCRIPTPATH/../systemd/sarc_scrapers_hifreq.timer /etc/systemd/system/sarc_scrapers_hifreq.timer
+ln -s -f $SCRIPTPATH/../systemd/sarc_scrapers_lowfreq.service /etc/systemd/system/sarc_scrapers_lowfreq.service
+ln -s -f $SCRIPTPATH/../systemd/sarc_scrapers_lowfreq.timer /etc/systemd/system/sarc_scrapers_lowfreq.timer
 ln -s -f $SCRIPTPATH/../systemd/sarc_containers.service /etc/systemd/system/sarc_containers.service
 ln -s -f $SCRIPTPATH/../systemd/sarc_backup.service /etc/systemd/system/sarc_backup.service
 ln -s -f $SCRIPTPATH/../systemd/sarc_backup.timer /etc/systemd/system/sarc_backup.timer
 systemctl daemon-reload
-systemctl start sarc_scrapers_daily.timer
-systemctl enable sarc_scrapers_daily.timer
-systemctl start sarc_scrapers_hourly.timer
-systemctl enable sarc_scrapers_hourly.timer
+systemctl start sarc_scrapers_hifreq.timer
+systemctl enable sarc_scrapers_hifreq.timer
+systemctl start sarc_scrapers_lowfreq.timer
+systemctl enable sarc_scrapers_lowfreq.timer
 systemctl start sarc_containers.service
 systemctl enable sarc_containers.service
 systemctl start sarc_backup.timer
