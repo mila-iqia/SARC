3d98ac79d2ba3f09a00d07bd7d8f8c4734357734
Bruno Carrez <bruno.carrez@mila.quebec>
2026-01-08 12:46:54 -0500

    update sarc-prod config

--- scripts/systemd/scrapers_lowfreq.sh
+++ scripts/systemd/scrapers_lowfreq.sh
@@ -3,7 +3,7 @@ SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd $SCRIPTPATH/../../
 # fetch prometheus
-sudo -u sarc SARC_MODE=scraping SARC_CONFIG=$SCRIPTPATH/../../config/sarc-prod.yaml ../.local/bin/uv run sarc acquire prometheus -c narval mila -a 1440
+sudo -u sarc SARC_MODE=scraping SARC_CONFIG=$SCRIPTPATH/../../config/sarc-prod.yaml ../.local/bin/uv run sarc acquire prometheus -c narval mila rorqual -a 1440
 # fetch users
 sudo -u sarc SARC_MODE=scraping SARC_CONFIG=$SCRIPTPATH/../../config/sarc-prod.yaml ../.local/bin/uv run sarc fetch users
 # parse users
