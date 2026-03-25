1d88b0a1c4f75df129333aeeb96746b3587d6c7e
Bruno Carrez <bruno.carrez@mila.quebec>
2026-03-25 17:49:40 -0400

    Create envvars.sh

---
+++ scripts/systemd/envvars.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export SARC_DB_CONNECTION="mongodb://writeuser:<replaceme>@localhost:27017/sarc"
+export SARC_DB_NAME="sarc"
+export SARC_CACHE_PATH="/home/sarc/SARC/sarc-cache"
+export SERIEUX_PASSWORD="<replaceme>"
+export SARC_MODE=scraping
+# export SARC_CONFIG="/home/sarc/SARC/config/sarc-config/"
