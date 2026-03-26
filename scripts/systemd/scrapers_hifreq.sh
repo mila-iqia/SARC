4058a604bcdc7977087532e8011bbab81eace0e1
nurbal <sarc@sarc01-dev.server.mila.quebec>
2026-03-26 09:58:18 -0400

    update scrapers script

--- scripts/systemd/scrapers_hifreq.sh
+++ scripts/systemd/scrapers_hifreq.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-source envvars.sh
-export SARC_CONFIG="/home/sarc/SARC/config/sarc-config/fetch_hourly.yaml"
+source scripts/systemd/envvars.sh
+export SARC_CONFIG="/home/sarc/SARC/config/sarc-config/fetch_hourly_sarc01-dev.yaml"
 # fetch jobs
 ../.local/bin/uv run sarc fetch jobs -c narval fir nibi rorqual mila -a 60
 ../.local/bin/uv run sarc parse jobs
