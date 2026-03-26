54be1ff6c4039b2f7bab1cd478b79960d06c6938
Bruno Carrez <bruno.carrez@mila.quebec>
2026-03-26 10:40:01 -0400

    Create scrapers_manual.sh

---
+++ scripts/systemd/scrapers_manual.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+source scripts/systemd/envvars.sh
+export SARC_CONFIG="/home/sarc/SARC/config/sarc-config/fetch_hourly_sarc01-dev.yaml"
+# fetch jobs
+../.local/bin/uv run sarc fetch jobs -c tamia -a 60
+../.local/bin/uv run sarc parse jobs
