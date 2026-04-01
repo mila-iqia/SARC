fb3e9f1a52e8c4fa341b89502abad958d015a336
Bruno Carrez <bruno.carrez@mila.quebec>
2026-04-01 13:25:01 -0400

    update slack message for manual scraping

--- scripts/systemd/scrapers_manual.sh
+++ scripts/systemd/scrapers_manual.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 source scripts/systemd/envvars.sh
-export SARC_CONFIG="/home/sarc/SARC/config/sarc-config/fetch_hourly_sarc01-dev.yaml"
+export SARC_CONFIG="/home/sarc/SARC/config/sarc-config/fetch_jobs_manual_sarc01-dev.yaml"
 # fetch jobs
 ../.local/bin/uv run sarc fetch jobs -c tamia -a 60 --max_intervals 24
 ../.local/bin/uv run sarc parse jobs
