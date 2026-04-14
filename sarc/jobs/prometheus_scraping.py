a72dac38d87214e421200fb5c298ac808363b4d9
Arnaud Bergeron <abergeron@gmail.com>
2026-04-14 11:02:59 -0400

    Avoid doing a db query for empty data

--- sarc/jobs/prometheus_scraping.py
+++ sarc/jobs/prometheus_scraping.py
@@ -107,6 +107,12 @@ def parse_prometheus(since: datetime | None, update_parsed_date: bool) -> None:
                 continue
             job_id = int(job_id_str)
             submit_time = datetime.fromisoformat(submit_time_str)
+            data = json.loads(value.decode("utf-8"))
+            if data == []:
+                logger.warning(
+                    f"Empty data found for job {job_id} on cluster {cluster_name} (submit_time {submit_time}), skipping cache entry"
+                )
+                continue
             entry = collection.find_one_by(
                 {
                     "cluster_name": cluster_name,
@@ -117,12 +123,6 @@ def parse_prometheus(since: datetime | None, update_parsed_date: bool) -> None:
             if entry is None:
                 logger.error("Could not find job for %s", key)
                 error = True
-                continue
-            data = json.loads(value.decode("utf-8"))
-            if data == []:
-                logger.warning(
-                    f"Empty data found for job {job_id} on cluster {cluster_name} (submit_time {submit_time}), skipping cache entry"
-                )
                 continue
             gpu_type = data[0]["metric"]["gpu_type"]
             need_save = False
