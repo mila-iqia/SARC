6b6a8739b840f5e03683b7688c686661cfd507b1
Bruno Carrez <bruno.carrez@mila.quebec>
2026-04-15 10:47:10 -0400

    Update scrapers_lowfreq.sh

--- scripts/systemd/scrapers_lowfreq.sh
+++ scripts/systemd/scrapers_lowfreq.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-source scripts/systemd/envvars.sh
+source envvars.sh
 export SARC_CONFIG="/home/sarc/SARC/config/sarc-config/fetch_daily_sarc01-dev.yaml"
 # fetch prometheus
-../.local/bin/uv run sarc acquire prometheus -c narval mila rorqual -a 1440
+../.local/bin/uv run sarc fetch prometheus -c narval mila rorqual -a 1440
+../.local/bin/uv run sarc parse prometheus
 # fetch users
 ../.local/bin/uv run sarc fetch users
 # parse users
