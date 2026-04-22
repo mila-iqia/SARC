89b9648e2a13472f79d071446e0ad26aad47de4f
Arnaud Bergeron <abergeron@gmail.com>
2026-04-22 14:41:33 -0400

    Add tentative db model for allocations

--- sarc/db/__init__.py
+++ sarc/db/__init__.py
@@ -1,2 +1,2 @@
 # ruff: noqa: F401
-from . import cluster, diskusage, job, users
+from . import allocation, cluster, diskusage, job, users
---
+++ sarc/db/allocation.py
@@ -0,0 +1,30 @@
+from datetime import date, datetime
+from typing import cast
+
+from pydantic import ByteSize
+from sqlmodel import Field, SQLModel
+
+
+class Allocation(SQLModel, table=True):
+    # Database ID
+    id: int | None = Field(default=None, primary_key=True)
+
+    cluster_name: str
+    resource_name: str
+    group_name: str
+    timestamp: datetime
+    start: date
+    end: date
+
+    gpu_year: int | None = 0
+    cpu_year: int | None = 0
+    rgu_year: int | None = 0
+    vcpu_year: int | None = 0
+    vgpu_year: int | None = 0
+    project_size: ByteSize | None = cast(ByteSize, 0)
+    project_inodes: float | None = 0
+    nearline: ByteSize | None = cast(ByteSize, 0)
+    dCache: ByteSize | None = cast(ByteSize, 0)
+    object: ByteSize | None = cast(ByteSize, 0)
+    cloud_volume: ByteSize | None = cast(ByteSize, 0)
+    cloud_shared: ByteSize | None = cast(ByteSize, 0)
