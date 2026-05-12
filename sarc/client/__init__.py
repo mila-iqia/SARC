29b800b8269ab48c903492d012be9a5466c68974
Arnaud Bergeron <abergeron@gmail.com>
2026-05-12 13:53:37 -0400

    Remove tests for functions that don't exist

--- sarc/client/__init__.py
+++ sarc/client/__init__.py
@@ -1,13 +1,3 @@
-from .series import (
-    compute_cost_and_waste,
-    compute_time_frames,
-    load_job_series,
-    update_job_series_rgu,
-)
+from .series import compute_time_frames
 
-__all__ = [
-    "load_job_series",
-    "compute_time_frames",
-    "compute_cost_and_waste",
-    "update_job_series_rgu",
-]
+__all__ = ["compute_time_frames"]
