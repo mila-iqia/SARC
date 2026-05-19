e4315ba2b399947950fb888db5efcb76b5c92697
Bruno Carrez <bruno@carrez.net>
2026-05-19 14:18:08 -0400

    mila_ldap, drac

--- sarc/users/drac.py
+++ sarc/users/drac.py
@@ -1,10 +1,14 @@
 import csv
+import json
 from collections.abc import Iterable
 from dataclasses import dataclass
 from datetime import datetime
 
 from serieux.features.encrypt import Secret
+from sqlmodel import func, select
 
+from sarc.config import config
+from sarc.db.users import CredentialsDB
 from sarc.scraping.users import (
     Credentials,
     MatchID,
@@ -14,60 +18,72 @@ from sarc.scraping.users import (
 )
 
 
-@dataclass
-class DRACRolesConfig:
-    csv: Secret[str]
-
-
 def _dict_to_lowercase[T](D: dict[str, T]) -> dict[str, T]:
     return dict((k.lower(), v) for (k, v) in D.items())
-
-
-class DRACRolesScraper(UserScraper[DRACRolesConfig]):
-    config_type = DRACRolesConfig
-
-    def get_user_data(self, config: DRACRolesConfig) -> bytes:
-        return config.csv.encode("utf-8")
-
-    def parse_user_data(self, data: bytes, cache_time: datetime) -> Iterable[UserMatch]:  # noqa: ARG002
-        for d in csv.DictReader(data.decode("utf-8").split("\n")):
-            d = _dict_to_lowercase(d)
-            yield UserMatch(
-                display_name=d["nom"],
-                email=d["email"],
-                matching_id=MatchID(name="drac_role", mid=d["ccri"][:-3]),
-            )
-
-
-_builtin_scrapers["drac_role"] = DRACRolesScraper()
 
 
 @dataclass
 class DRACMemberConfig:
     csv: Secret[str]
+    csv_date: datetime
 
 
 class DRACMemberScraper(UserScraper[DRACMemberConfig]):
     config_type = DRACMemberConfig
 
     def get_user_data(self, config: DRACMemberConfig) -> bytes:
-        return config.csv.encode("utf-8")
+        return json.dumps(
+            {"csv": config.csv, "csv_date": config.csv_date.isoformat()}
+        ).encode("utf-8")
 
-    def parse_user_data(self, data: bytes, cache_time: datetime) -> Iterable[UserMatch]:
-        for d in csv.DictReader(data.decode("utf-8").split("\n")):
+    def parse_user_data(self, data: bytes, cache_time: datetime) -> Iterable[UserMatch]:  # noqa: ARG002
+        try:
+            j = json.loads(data.decode("utf-8"))
+            # new cache format, data is a JSON containing a dict {csv, csv_date}
+            yield from parse_csv(j["csv"], j["csv_date"])
+
+        except json.JSONDecodeError:
+            # old cache, data is the CSV
+            yield from parse_csv(data.decode("utf-8"), None)
+
+
+def parse_csv(csv_file: str, csv_date: datetime | None) -> Iterable[UserMatch]:
+
+    with config().db.session() as sess:
+        for d in csv.DictReader(csv_file.split("\n")):
             d = _dict_to_lowercase(d)
             creds = Credentials()
-            creds.insert(
-                d["username"],
-                start=datetime.strptime(d["member_since"], "%Y-%m-%d %H:%M:%S %z"),
-                end=cache_time,
-            )
-            yield UserMatch(
-                display_name=d["name"],
-                email=d["email"],
-                matching_id=MatchID(name="drac_member", mid=d["ccri"][:-3]),
-                associated_accounts={"drac": creds},
-            )
+            if d["activation_status"] in ["activated", "recently_renewed"]:
+                creds.insert(
+                    d["username"],
+                    start=datetime.strptime(d["member_since"], "%Y-%m-%d %H:%M:%S %z"),
+                )
+            elif (
+                csv_date is not None
+                and sess.exec(
+                    select(
+                        select(CredentialsDB)
+                        .where(
+                            CredentialsDB.domain == "drac",
+                            CredentialsDB.username == d["username"],
+                            func.upper_inf(CredentialsDB.valid),
+                        )
+                        .exists()
+                    )
+                ).one()
+            ):
+                creds.insert(
+                    d["username"],
+                    start=datetime.strptime(d["member_since"], "%Y-%m-%d %H:%M:%S %z"),
+                    end=csv_date,
+                )
+            if creds.values != []:
+                yield UserMatch(
+                    display_name=d["name"],
+                    email=d["email"],
+                    matching_id=MatchID(name="drac_member", mid=d["ccri"][:-3]),
+                    associated_accounts={"drac": creds},
+                )
 
 
 _builtin_scrapers["drac_member"] = DRACMemberScraper()
--- tests/unittests/users/test_drac.py
+++ tests/unittests/users/test_drac.py
@@ -5,41 +5,8 @@ from pathlib import Path
 
 import pytest
 
-from sarc.users.drac import (
-    DRACMemberConfig,
-    DRACMemberScraper,
-    DRACRolesConfig,
-    DRACRolesScraper,
-)
+from sarc.users.drac import DRACMemberConfig, DRACMemberScraper
 from tests.unittests.core.test_users_scraping import UserPluginTester
-
-
-class TestDRACRolesScraper(UserPluginTester):
-    plugin = DRACRolesScraper()
-
-    data = (Path(__file__).parent / "inputs" / "drac_roles.csv").read_text()
-    raw_config = {"csv": data}
-    parsed_config = DRACRolesConfig(csv=data)
-
-    def test_fetch_data(self, data_regression):
-        config = DRACMemberConfig(csv=self.data)
-        data = self.plugin.get_user_data(config)
-        # Uncomment to update the input data for the parse test
-        # with open(Path(__file__).parent / "inputs" / "drac_role1.cache", "wb") as f:
-        #    f.write(data)
-        data_regression.check(data.decode(), basename="test_fetch_roles")
-
-    @pytest.mark.parametrize("raw_file", ["drac_role1.cache"])
-    def test_parse_data(self, raw_file, data_regression):
-        with open(Path(__file__).parent / "inputs" / raw_file, "rb") as f:
-            raw_data = f.read()
-        data = list(
-            d.model_dump()
-            for d in self.plugin.parse_user_data(
-                raw_data, datetime(year=2024, month=1, day=1, tzinfo=UTC)
-            )
-        )
-        data_regression.check(data, basename="test_parse_roles")
 
 
 class TestDRACMemberScraper(UserPluginTester):
