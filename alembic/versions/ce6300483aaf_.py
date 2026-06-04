6f33511cbc7d2112edaa5a577a16170eeecaeee4
notoraptor <stevenbocco@gmail.com>
2026-06-04 16:57:47 -0400

    Update alembic

--- alembic/versions/ce6300483aaf_.py
+++ alembic/versions/ce6300483aaf_.py
@@ -10,12 +10,11 @@ from typing import Sequence, Union
 
 import sqlalchemy as sa
 import sqlmodel.sql.sqltypes
-
 from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "ce6300483aaf"
-down_revision: Union[str, Sequence[str], None] = "e48cef9efb1e"
+down_revision: Union[str, Sequence[str], None] = "23cb46269fc9"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
