b6e994995d9d358632e35fc386ae7028784a0596
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2026-06-17 16:05:44 -0400

    chore(deploy): remove systemd scripts for underusage notifications

--- scripts/systemd/notify_underusage.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
-cd $SCRIPTPATH/../../
-# send weekly underusage notifications (digest + researcher DMs when send_dms: true)
-sudo -u sarc SARC_MODE=client SARC_CONFIG=$SCRIPTPATH/../../config/sarc-prod.yaml ../.local/bin/uv run sarc notify underusage --send
